9b9718f46b28582fc26e40368f90f9d48c2ab631
siduck76 <siduckk76@protonmail.com>
2021-03-23 10:22:29 +0530

    show completion item doc popup

--- lua/nvim-compe/lua.lua
+++ lua/nvim-compe/lua.lua
@@ -15,7 +15,7 @@ require "compe".setup {
     max_abbr_width = 100,
     max_kind_width = 100,
     max_menu_width = 100,
-    documentation = false,
+    documentation = true,
     source = {
         path = true,
         buffer = true,
