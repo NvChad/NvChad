40cc37d23fd8a065d856b42016ebdb68082fa7cc
jaydamani <jaydamani567@gmail.com>
2021-03-31 15:31:05 +0530

    added tab completion

--- lua/nvim-compe/lua.lua
+++ lua/nvim-compe/lua.lua
@@ -33,6 +33,16 @@ require "compe".setup {
 local t = function(str)
     return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
+
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return vim.fn['compe#complete']()
+  end
+end
 _G.s_tab_complete = function()
     if vim.fn.pumvisible() == 1 then
         return t "<C-p>"
@@ -45,6 +55,7 @@ end
 
 --  mappings
 
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
