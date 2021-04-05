0587437cdf18f95049ac211acbc0e22a08581c85
Alexander Serowy <serowy@hotmail.com>
2021-04-05 08:43:12 +0200

    removed automatic PackerInstall

--- install.sh
+++ install.sh
@@ -7,12 +7,10 @@ heading() {
 }
 
 heading "installing packer"
+rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 heading "linking config"
 rm -rf ~/.config/nvim/ && mkdir -p ~/.config/nvim
 cp -r init.lua ~/.config/nvim && cp -r lua ~/.config/nvim 
-
-heading "installing plugins"
-nvim --headless +PackerInstall +qa
 
