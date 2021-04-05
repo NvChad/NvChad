ff14bb4e50baa3e62043bc614e68b19be0893db8
Alexander Serowy <serowy@hotmail.com>
2021-04-05 09:06:12 +0200

    added automatic install for packer

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ heading() {
   echo $pfx $1
 }
 
-heading "installing packer"
-rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
 heading "linking config"
 rm -rf ~/.config/nvim/ && mkdir -p ~/.config/nvim
 cp -r init.lua ~/.config/nvim && cp -r lua ~/.config/nvim 
--- lua/pluginsList/lua.lua
+++ lua/pluginsList/lua.lua
@@ -1,35 +1,45 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute 'packadd packer.nvim'
+end
+
 -- check if packer is installed (~/local/share/nvim/site/pack)
 local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
 
 -- using { } when using a different branch of the plugin or loading the plugin with certain commands
 return require("packer").startup(
-    function()
-        use {"wbthomason/packer.nvim", opt = true}
-        use {"lukas-reineke/indent-blankline.nvim", branch = "lua"}
+function()
+  use {"wbthomason/packer.nvim", opt = true}
+  use {"lukas-reineke/indent-blankline.nvim", branch = "lua"}
 
-        use "norcalli/nvim-base16.lua"
-        use "kyazdani42/nvim-web-devicons"
-        use "kyazdani42/nvim-tree.lua"
-        use "nvim-lua/plenary.nvim"
-        use "lewis6991/gitsigns.nvim"
-        use "akinsho/nvim-bufferline.lua"
-        use "glepnir/galaxyline.nvim"
-        use "907th/vim-auto-save"
-        use "nvim-treesitter/nvim-treesitter"
-        use "norcalli/nvim-colorizer.lua"
-        use "ryanoasis/vim-devicons"
-        use "sbdchd/neoformat"
-        use "neovim/nvim-lspconfig"
-        use "kabouzeid/nvim-lspinstall"
-        use "hrsh7th/nvim-compe"
-        use "windwp/nvim-autopairs"
-        use "alvan/vim-closetag"
-        use "tweekmonster/startuptime.vim"
-        use "onsails/lspkind-nvim"
-        use "nvim-telescope/telescope.nvim"
-        use "nvim-telescope/telescope-media-files.nvim"
-        use "nvim-lua/popup.nvim"
-        use "karb94/neoscroll.nvim"
-        
-    end
+  use "norcalli/nvim-base16.lua"
+  use "kyazdani42/nvim-web-devicons"
+  use "kyazdani42/nvim-tree.lua"
+  use "nvim-lua/plenary.nvim"
+  use "lewis6991/gitsigns.nvim"
+  use "akinsho/nvim-bufferline.lua"
+  use "glepnir/galaxyline.nvim"
+  use "907th/vim-auto-save"
+  use "nvim-treesitter/nvim-treesitter"
+  use "norcalli/nvim-colorizer.lua"
+  use "ryanoasis/vim-devicons"
+  use "sbdchd/neoformat"
+  use "neovim/nvim-lspconfig"
+  use "kabouzeid/nvim-lspinstall"
+  use "hrsh7th/nvim-compe"
+  use "windwp/nvim-autopairs"
+  use "alvan/vim-closetag"
+  use "tweekmonster/startuptime.vim"
+  use "onsails/lspkind-nvim"
+  use "nvim-telescope/telescope.nvim"
+  use "nvim-telescope/telescope-media-files.nvim"
+  use "nvim-lua/popup.nvim"
+  use "karb94/neoscroll.nvim"
+
+end
 )
