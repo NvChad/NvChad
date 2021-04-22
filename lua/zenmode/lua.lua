86431978d9292632f97e485cf41a06ac61f3da93
siduck76 <siduckk76@protonmail.com>
2021-04-22 18:57:36 +0530

    TrueZen.nvim : add top & bottom padding

--- lua/zenmode/lua.lua
+++ lua/zenmode/lua.lua
@@ -1,7 +1,14 @@
-require("true-zen").setup(
+local true_zen = require("true-zen")
+
+true_zen.setup(
     {
         true_false_commands = false,
         cursor_by_mode = false,
+        before_minimalist_mode_shown = true,
+        before_minimalist_mode_hidden = true,
+        after_minimalist_mode_shown = true,
+        after_minimalist_mode_hidden = true,
+        unknown_bkg_color_fix = true,
         bottom = {
             hidden_laststatus = 0,
             hidden_ruler = false,
@@ -27,7 +34,14 @@ require("true-zen").setup(
             shown_signcolumn = "no"
         },
         ataraxis = {
-            left_right_padding = 40
+            just_do_it_for_me = true,
+            left_padding = 40,
+            right_padding = 40,
+            top_padding = 2,
+            bottom_padding = 2
+        },
+        integrations = {
+            integration_galaxyline = true
         }
     }
 )
