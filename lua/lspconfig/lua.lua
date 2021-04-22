5716759f40da7fe0cfa66232fb37d0ee43e8b559
siduck76 <siduckk76@protonmail.com>
2021-04-22 23:13:06 +0530

    clean lspconfig

--- lua/lspconfig/lua.lua
+++ lua/lspconfig/lua.lua
@@ -2,9 +2,10 @@ vim.cmd [[packadd nvim-lspconfig]]
 vim.cmd [[packadd nvim-compe]]
 
 function on_attach(client)
-    local function buf_set_keymap(...)
+    local function map(...)
         vim.api.nvim_buf_set_keymap(bufnr, ...)
     end
+
     local function buf_set_option(...)
         vim.api.nvim_buf_set_option(bufnr, ...)
     end
@@ -12,28 +13,28 @@ function on_attach(client)
     buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
     -- Mappings.
-    local opts = {noremap = true, silent = true}
-    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-    buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-    buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+    local opts = {noremap = true, silent = true, normal = true}
+    map("gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    map("gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    map("K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    map("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    map("<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    map("<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    map("<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    map("<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+    map("<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    map("<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    map("gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    map("<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+    map("[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+    map("]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+    map("<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 
     -- Set some keybinds conditional on server capabilities
     if client.resolved_capabilities.document_formatting then
-        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+        map("<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
     elseif client.resolved_capabilities.document_range_formatting then
-        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+        map("<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
     end
 end
 
@@ -69,7 +70,10 @@ require "lspconfig".sumneko_lua.setup {
                 globals = {"vim"}
             },
             workspace = {
-                library = {[vim.fn.expand("$VIMRUNTIME/lua")] = true, [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true}
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
+                }
             },
             telemetry = {
                 enable = false
