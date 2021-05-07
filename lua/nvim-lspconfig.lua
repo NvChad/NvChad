122e89e36158a95320e8534605a0ccc0379a7aa8
siduck76 <siduck13@gmail.com>
2021-05-07 13:46:04 +0530

    added ccls lsp

--- lua/nvim-lspconfig.lua
+++ lua/nvim-lspconfig.lua
@@ -36,7 +36,7 @@ function on_attach(client)
 end
 
 local lspconf = require "lspconfig"
-local servers = {"html", "cssls", "tsserver", "pyright", "bashls", "vls", "clangd"}
+local servers = {"html", "cssls", "tsserver", "pyright", "bashls", "vls", "clangd", "ccls}
 
 for k, lang in pairs(servers) do
     lspconf[lang].setup {
