2fe776a3866b49dc6bde0b5315c777855a195838
siduck76 <siduckk76@protonmail.com>
2021-05-11 09:12:49 +0530

    nvim-lspconfig : fix some mistakes!

--- lua/nvim-lspconfig.lua
+++ lua/nvim-lspconfig.lua
@@ -1,8 +1,7 @@
 function on_attach(client)
-    local function map(...)
+    local function buf_set_keymap(...)
         vim.api.nvim_buf_set_keymap(bufnr, ...)
     end
-
     local function buf_set_option(...)
         vim.api.nvim_buf_set_option(bufnr, ...)
     end
@@ -10,44 +9,48 @@ function on_attach(client)
     buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
     -- Mappings.
-    local opts = {noremap = true, silent = true, normal = true}
-    map("gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    map("gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    map("K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    map("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    map("<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    map("<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    map("<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    map("<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    map("<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    map("<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    map("gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    map("<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-    map("[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-    map("]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-    map("<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+    local opts = {noremap = true, silent = true}
+
+    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+    buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+    buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+    buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 
     -- Set some keybinds conditional on server capabilities
     if client.resolved_capabilities.document_formatting then
-        map("<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
     elseif client.resolved_capabilities.document_range_formatting then
-        map("<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
     end
 end
 
-local lspconf = require "lspconfig"
-local servers = {"html", "cssls", "tsserver", "pyright", "bashls", "vls", "clangd", "ccls"}
+local lspconf = require("lspconfig")
 
-for k, lang in pairs(servers) do
+-- these langs require same lspconfig so put em all in a table and loop through!
+local servers = {"html", "cssls", "tsserver", "pyright", "bashls", "clangd", "ccls"}
+
+for _, lang in ipairs(servers) do
     lspconf[lang].setup {
+        on_attach = on_attach,
         root_dir = vim.loop.cwd
     }
 end
 
--- vls example
-local vls_binary = '/usr/local/bin/vls'
-require'lspconfig'.vls.setup {
-  cmd = {vls_binary},
+-- vls conf example
+local vls_binary = "/usr/local/bin/vls"
+lspconf.vls.setup {
+    cmd = {vls_binary}
 }
 
 -- lua lsp settings
@@ -56,7 +59,7 @@ USER = "/home/" .. vim.fn.expand("$USER")
 local sumneko_root_path = USER .. "/.config/lua-language-server"
 local sumneko_binary = USER .. "/.config/lua-language-server/bin/Linux/lua-language-server"
 
-require "lspconfig".sumneko_lua.setup {
+lspconf.sumneko_lua.setup {
     cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
     root_dir = function()
         return vim.loop.cwd()
