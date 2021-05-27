355c2cb33c76ecd2628d5bb0915fd14f6887ebcf
siduck76 <siduckk76@protonmail.com>
2021-05-27 15:20:30 +0530

    clean compe config since it supports lazy loading

--- lua/compe-completion.lua
+++ lua/compe-completion.lua
@@ -14,16 +14,9 @@ require "compe".setup {
     max_menu_width = 100,
     documentation = true,
     source = {
-        path = true,
         buffer = {kind = "﬘", true},
-        calc = true,
         vsnip = {kind = "﬌"}, --replace to what sign you prefer
-        nvim_lsp = true,
-        nvim_lua = true,
-        spell = true,
-        tags = true,
-        snippets_nvim = true,
-        treesitter = true
+        nvim_lsp = true
     }
 }
 
@@ -79,24 +72,3 @@ function _G.completions()
 end
 
 vim.api.nvim_set_keymap("i", "<CR>", "v:lua.completions()", {expr = true})
-
-local g = vim.g
-
--- speeden up compe
-g.loaded_compe_calc = 0
-g.loaded_compe_emoji = 0
-
-g.loaded_compe_luasnip = 0
-g.loaded_compe_nvim_lua = 0
-
-g.loaded_compe_path = 0
-g.loaded_compe_spell = 0
-g.loaded_compe_tags = 0
-g.loaded_compe_treesitter = 0
-
-g.loaded_compe_snippets_nvim = 0
-
-g.loaded_compe_ultisnips = 0
-g.loaded_compe_vim_lsc = 0
-g.loaded_compe_vim_lsp = 0
-g.loaded_compe_omni = 0
