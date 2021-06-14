a5b665f4a88a6af660f20cfd0b3ce10ad595ca40
siduck76 <siduckk76@protonmail.com>
2021-06-14 09:24:47 +0530

    dashboard config

---
+++ lua/dashboard.lua
@@ -0,0 +1,28 @@
+local g = vim.g
+
+g.dashboard_default_executive = "telescope"
+g.dashboard_custom_header = {
+    "                                   ",
+    "                                   ",
+    "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆         ",
+    "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
+    "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
+    "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
+    "          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
+    "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
+    "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
+    " ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
+    " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
+    "    ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆       ",
+    "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+    "                                   "
+}
+
+g.dashboard_custom_section = {
+    a = {description = {"  Find File                 SPC f f"}, command = "Telescope find_files"},
+    b = {description = {"  Recents                   SPC f o"}, command = "Telescope oldfiles"},
+    c = {description = {"  Find Word                 SPC f w"}, command = "Telescope live_grep"},
+    d = {description = {"洛 New File                  SPC f b"}, command = "DashboardNewFile"},
+    e = {description = {"󰓏  Bookmarks                 SPC f b"}, command = "Telescope marks"},
+    f = {description = {"  Load Last Session         SPC f s"}, command = "SessionLoad"}
+}
