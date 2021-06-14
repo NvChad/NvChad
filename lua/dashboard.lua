b71c257bdc15622da1a6f8726be8e2040753435f
siduck76 <siduckk76@protonmail.com>
2021-06-14 10:56:26 +0530

    Update dashboard.lua

--- lua/dashboard.lua
+++ lua/dashboard.lua
@@ -22,7 +22,7 @@ g.dashboard_custom_section = {
     a = {description = {"  Find File                 SPC f f"}, command = "Telescope find_files"},
     b = {description = {"  Recents                   SPC f o"}, command = "Telescope oldfiles"},
     c = {description = {"  Find Word                 SPC f w"}, command = "Telescope live_grep"},
-    d = {description = {"洛 New File                  SPC f b"}, command = "DashboardNewFile"},
+    d = {description = {"洛 New File                  SPC f n"}, command = "DashboardNewFile"},
     e = {description = {"󰓏  Bookmarks                 SPC f b"}, command = "Telescope marks"},
     f = {description = {"  Load Last Session         SPC f s"}, command = "SessionLoad"}
 }
