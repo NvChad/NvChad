0dbc1c88bff841cef822ae4ef0e35b47bbff8862
Henri <52706578+Vanderscycle@users.noreply.github.com>
2021-06-14 18:35:16 -0700

    Create emmet.lua
    Lsp install is great but I wanted emmet to help me speed up writting html and css. I need to find a way to allow it when using vue or svelte.

---
+++ lua/lsp/emmet.lua
@@ -0,0 +1,19 @@
+local lspconfig = requirelocal nvim_lsp = require'lspconfig'
+local configs = require'lspconfig/configs'
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+configs.emmet_ls = {
+  default_config = {
+    cmd = {'emmet-ls', '--stdio'};
+    filetypes = {'html', 'css'};
+    root_dir = function()
+      return vim.loop.cwd()
+    end;
+    settings = {};
+  };
+}
+
+nvim_lsp.emmet_ls.setup{
+  -- on_attach = on_attach;
+}
