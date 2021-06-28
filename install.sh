4dde80f3155c0587cdc0fcc316f2614797c1ef9b
siduck76 <siduckk76@protonmail.com>
2021-06-28 21:23:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ get_platform() {
   case "$(uname -s)" in
   Linux*) platform=Linux ;;
   Darwin*) platform=Mac ;;
-  CYGWIN*) platform=Cygwin ;;
-  MINGW*) platform=MinGw ;;
   *) platform="UNKNOWN:${unameOut}" ;;
   esac
   echo $platform
