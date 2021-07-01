9dbc9e68807273e0890faf9d825e26d30b727c0e
ashincoder <83629316+ashincoder@users.noreply.github.com>
2021-07-01 21:30:03 +0530

    Updated telescope config
    fixes issue #115.

--- lua/telescope-nvim.lua
+++ lua/telescope-nvim.lua
@@ -12,32 +12,33 @@ M.config = function()
                 "--column",
                 "--smart-case"
             },
-            prompt_position = "top",
-            prompt_prefix = "   ",
-            selection_caret = "  ",
-            entry_prefix = "  ",
-            initial_mode = "insert",
-            selection_strategy = "reset",
-            sorting_strategy = "descending",
-            layout_strategy = "horizontal",
-            layout_defaults = {
-                horizontal = {
-                    mirror = false,
-                    preview_width = 0.55
-                },
-                vertical = {
-                    mirror = false
-                }
-            },
-            file_sorter = require "telescope.sorters".get_fuzzy_file,
-            file_ignore_patterns = {},
-            generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
-            shorten_path = true,
-            winblend = 0,
-            width = 0.75,
-            preview_cutoff = 120,
-            results_height = 1,
-            results_width = 0.8,
+          },
+            layout_config = {
+
+              width = 0.8,
+              height = 1,
+              prompt_position = "top",
+              prompt_prefix = "   ",
+              selection_caret = "  ",
+              entry_prefix = "  ",
+              initial_mode = "insert",
+              selection_strategy = "reset",
+              sorting_strategy = "descending",
+              layout_strategy = "horizontal",
+              winblend = 0,
+              shorten_path = true,
+              preview_cutoff = 120,
+              file_sorter = require "telescope.sorters".get_fuzzy_file,
+              file_ignore_patterns = {},
+              generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
+              horizontal = {
+                  mirror = false,
+                  preview_width = 0.55
+              },
+              vertical = {
+                  mirror = false
+              },
+          },
             border = {},
             borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
             color_devicons = true,
@@ -48,21 +49,20 @@ M.config = function()
             qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
             -- Developer configurations: Not meant for general override
             buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker
-        },
-        extensions = {
-            fzf = {
-                fuzzy = true, -- false will only do exact matching
-                override_generic_sorter = false, -- override the generic sorter
-                override_file_sorter = true, -- override the file sorter
-                case_mode = "smart_case" -- or "ignore_case" or "respect_case"
-                -- the default case_mode is "smart_case"
-            },
-            media_files = {
-                filetypes = {"png", "webp", "jpg", "jpeg"},
-                find_cmd = "rg" -- find command (defaults to `fd`)
-            }
-        }
-    }
+          }
+            extensions = {
+                fzf = {
+                    fuzzy = true, -- false will only do exact matching
+                    override_file_sorter = true, -- override the file sorter
+                    override_generic_sorter = false, -- override the generic sorter
+                    case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                },
+                media_files = {
+                    filetypes = {"png", "webp", "jpg", "jpeg"},
+                    find_cmd = "rg" -- find command (defaults to `fd`)
+                }
+          }
 
     require("telescope").load_extension("fzf")
     require("telescope").load_extension("media_files")
