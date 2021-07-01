dfe707d6db15e67a81e7e3b98674db9e03daaba0
siduck76 <siduck13@gmail.com>
2021-07-01 21:38:46 +0530

    Update telescope-nvim.lua

--- lua/telescope-nvim.lua
+++ lua/telescope-nvim.lua
@@ -12,33 +12,31 @@ M.config = function()
                 "--column",
                 "--smart-case"
             },
-          },
+            prompt_prefix = "   ",
+            selection_caret = "  ",
+            entry_prefix = "  ",
+            initial_mode = "insert",
+            selection_strategy = "reset",
+            sorting_strategy = "descending",
+            layout_strategy = "horizontal",
             layout_config = {
-
-              width = 0.8,
-              height = 1,
-              prompt_position = "top",
-              prompt_prefix = "   ",
-              selection_caret = "  ",
-              entry_prefix = "  ",
-              initial_mode = "insert",
-              selection_strategy = "reset",
-              sorting_strategy = "descending",
-              layout_strategy = "horizontal",
-              winblend = 0,
-              shorten_path = true,
-              preview_cutoff = 120,
-              file_sorter = require "telescope.sorters".get_fuzzy_file,
-              file_ignore_patterns = {},
-              generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
-              horizontal = {
-                  mirror = false,
-                  preview_width = 0.55
-              },
-              vertical = {
-                  mirror = false
-              },
-          },
+                horizontal = {
+                    prompt_position = "top",
+                    preview_width = 0.55,
+                    results_width = 0.8
+                },
+                vertical = {
+                    mirror = false
+                },
+                width = 0.87,
+                height = 0.80,
+                preview_cutoff = 120
+            },
+            file_sorter = require "telescope.sorters".get_fuzzy_file,
+            file_ignore_patterns = {},
+            generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
+            shorten_path = true,
+            winblend = 0,
             border = {},
             borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
             color_devicons = true,
@@ -49,20 +47,21 @@ M.config = function()
             qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
             -- Developer configurations: Not meant for general override
             buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker
-          }
-            extensions = {
-                fzf = {
-                    fuzzy = true, -- false will only do exact matching
-                    override_file_sorter = true, -- override the file sorter
-                    override_generic_sorter = false, -- override the generic sorter
-                    case_mode = "smart_case" -- or "ignore_case" or "respect_case"
-                    -- the default case_mode is "smart_case"
-                },
-                media_files = {
-                    filetypes = {"png", "webp", "jpg", "jpeg"},
-                    find_cmd = "rg" -- find command (defaults to `fd`)
-                }
-          }
+        },
+        extensions = {
+            fzf = {
+                fuzzy = true, -- false will only do exact matching
+                override_generic_sorter = false, -- override the generic sorter
+                override_file_sorter = true, -- override the file sorter
+                case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+                -- the default case_mode is "smart_case"
+            },
+            media_files = {
+                filetypes = {"png", "webp", "jpg", "jpeg"},
+                find_cmd = "rg" -- find command (defaults to `fd`)
+            }
+        }
+    }
 
     require("telescope").load_extension("fzf")
     require("telescope").load_extension("media_files")
