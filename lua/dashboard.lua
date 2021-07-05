f76ad7da19d4e98b864d5b9442dde8286855e698
ashincoder <ashinant15@gmail.com>
2021-07-05 20:41:02 +0530

    added special telescope mapping for neovim config

--- lua/dashboard.lua
+++ lua/dashboard.lua
@@ -28,9 +28,10 @@ M.config = function()
         a = {description = {"  Find File                 SPC f f"}, command = "Telescope find_files"},
         b = {description = {"  Recents                   SPC f o"}, command = "Telescope oldfiles"},
         c = {description = {"  Find Word                 SPC f w"}, command = "Telescope live_grep"},
-        d = {description = {"洛 New File                  SPC f n"}, command = "DashboardNewFile"},
-        e = {description = {"  Bookmarks                 SPC b m"}, command = "Telescope marks"},
-        f = {description = {"  Load Last Session         SPC s l"}, command = "SessionLoad"}
+        d = {description = {"洛 New File                   SPC f n"}, command = "DashboardNewFile"},
+        e = {description = {"  Open Config               SPC f l"}, command = "Telescope find_files cwd=~/.config/nvim"},
+        f = {description = {"  Bookmarks                 SPC b m"}, command = "Telescope marks"},
+        g = {description = {"  Load Last Session         SPC s l"}, command = "SessionLoad"}
     }
 
     g.dashboard_custom_footer = {
