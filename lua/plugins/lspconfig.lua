9f8ce72e553e85ba6a42b7cd083688455890dd91
siduck76 <siduckk76@protonmail.com>
2021-07-09 09:19:24 +0530

    add preload sizes for lua lsp (#143)

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -64,7 +64,9 @@ M.config = function()
                                 library = {
                                     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
                                     [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
-                                }
+                                },
+                                maxPreload = 100000,
+                                preloadFileSize = 10000
                             },
                             telemetry = {
                                 enable = false
