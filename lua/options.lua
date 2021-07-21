020daec5fc827bfd03bd92e1f42b6ed569af6ec9
siduck76 <siduckk76@protonmail.com>
2021-07-21 23:06:28 +0530

    disable matchit
    matchit was already disabled by default but a recent PR enabled it again , sorry for that! Since vim matchup already does what matchit does , I'll remove it.

--- lua/options.lua
+++ lua/options.lua
@@ -63,7 +63,8 @@ local disabled_built_ins = {
     "2html_plugin",
     "logipat",
     "rrhelper",
-    "spellfile_plugin"
+    "spellfile_plugin",
+    "matchit"
 }
 
 for _, plugin in pairs(disabled_built_ins) do
