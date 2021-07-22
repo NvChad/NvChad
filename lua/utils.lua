0d69a528a8e2585f4feb6b61f23d393dc5d76300
Galen Rowell <growell3@gmail.com>
2021-07-22 18:04:21 +1000

    Fix statusline issue

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,7 +1,7 @@
 -- hide line numbers , statusline in specific buffers!
 vim.api.nvim_exec(
     [[
-   au TermOpen term://* setlocal nonumber laststatus=0
+   au TermOpen term://* setlocal nonumber
    au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif
 ]],
     false
