6b4435caef3d2f5568896758f790890318500f08
Akianonymus <anonymus.aki@gmail.com>
2021-07-23 20:08:20 +0530

    Don't use new_async
    It doesn't seem to be improving stuff as it is not supposed to
    
    Due to the half baked stats of the startuptime plugin, people were fooled
    
    no point in disabling filetype and whatenot
    
    See some discussion here: https://github.com/siduck76/NvChad/issues/175

--- init.lua
+++ init.lua
@@ -1,21 +1,10 @@
-require "options"
-
 local chad_modules = {
+    "options",
     "pluginList",
     "mappings",
     "utils"
 }
 
-local async
-async =
-    vim.loop.new_async(
-    vim.schedule_wrap(
-        function()
-            for i = 1, #chad_modules, 1 do
-                 pcall(require, chad_modules[i])          
-            end
-            async:close()
-        end
-    )
-)
-async:send()
+for i = 1, #chad_modules, 1 do
+    pcall(require, chad_modules[i])
+end
