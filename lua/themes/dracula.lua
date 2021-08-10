ae30324ed4edf8079208b234e35a6383f96080d5
Iorvethe <58810330+Iorvethe@users.noreply.github.com>
2021-08-10 14:19:33 +0300

    Add theme: Dracula (#225)
    * Add theme: Dracula
    
    * Dracula theme: make black2 lighter

---
+++ lua/themes/dracula.lua
@@ -0,0 +1,33 @@
+local colors = {
+    white = "#F8F8F2",
+    darker_black = "#1E2029",
+    black = "#282A36", --  nvim bg
+    black2 = "#303341",
+    one_bg = "#373844", -- real bg of onedark
+    one_bg2 = "#44475a",
+    one_bg3 = "#565761",
+    grey = "#6272a4",
+    grey_fg = "#b6b6b2",
+    grey_fg2 = "#ccccc7",
+    light_grey = "#f8f8f2",
+    red = "#FF5555",
+    baby_pink = "#DE8C92",
+    pink = "#FF79C6",
+    line = "#373844", -- for lines like vertsplit
+    green = "#50FA7B",
+    vibrant_green = "#69FF94",
+    nord_blue = "#D6ACFF",
+    blue = "#BD93F9",
+    yellow = "#F1FA8C",
+    sun = "#FFFFA5",
+    purple = "#BD93F9",
+    dark_purple = "#BD93F9",
+    teal = "#0088cc",
+    orange = "#FFB86C",
+    cyan = "#8BE9FD",
+    statusline_bg = "#373844",
+    lightbg = "#44475a",
+    lightbg2 = "#565761"
+}
+
+return colors
