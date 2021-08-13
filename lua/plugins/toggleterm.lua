91c8736146a9371bd97befacf55486b9416d154b
Galen Rowell <growell3@gmail.com>
2021-08-14 01:59:17 +1000

    WIP - Added toggleterm, based upon curtanips work (#237)
    * Added toggleterm, based upon curtanips work
    Also other various term fixes & changed term mappings
    
    * new NvChad config to hide statusline by filetype

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,33 @@
+local present, toggleterm = pcall(require, "toggleterm")
+if not present then
+    return
+end
+
+toggleterm.setup {
+      -- size can be a number or function which is passed the current terminal
+    size = function(term)
+        if term.direction == "horizontal" then
+            return 15
+        elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+        end
+    end,
+    -- open_mapping = [[<C-\>]], -- mapping set in mappings.lua
+    hide_numbers = true, -- hide the number column in toggleterm buffers
+    shade_terminals = false,
+    start_in_insert = true,
+    -- insert_mappings = true, -- see 'open_mapping', not set on purpose
+        -- whether or not the open mapping applies in insert mode
+    persist_size = true,
+    direction = 'vertical',
+    close_on_exit = true, -- close the terminal window when the process exits
+    -- This field is only relevant if direction is set to 'float'
+    float_opts = {
+        border = 'single',
+        winblend = 0,
+        highlights = {
+            border = "Normal",
+            background = "Normal",
+        }
+    }
+}
