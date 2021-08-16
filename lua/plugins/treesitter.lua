8e2ade1bb8169f025e42df429a2229a737325b6c
Galen Rowell <growell3@gmail.com>
2021-08-16 17:33:24 +1000

    chore: formatted with stylua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 local present, ts_config = pcall(require, "nvim-treesitter.configs")
 if not present then
-    return
+   return
 end
 
 ts_config.setup {
-    ensure_installed = {
-        "bash",
-        "lua"
-    },
-    highlight = {
-        enable = true,
-        use_languagetree = true
-    }
+   ensure_installed = {
+      "bash",
+      "lua",
+   },
+   highlight = {
+      enable = true,
+      use_languagetree = true,
+   },
 }
