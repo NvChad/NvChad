a6d9592a53c6c9eff105f5e7ab145982c6b9ad70
ashincoder <83629316+ashincoder@users.noreply.github.com>
2021-08-19 21:38:51 +0530

    feat: Added highlight on yank.

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,4 @@
+local utils = require "utils"
 local opt = vim.opt
 local g = vim.g
 
@@ -81,6 +82,16 @@ vim.cmd [[ au TermOpen term://* setlocal nonumber norelativenumber ]]
 -- Don't show status line on certain windows
 vim.cmd [[ au TermOpen term://* setfiletype terminal ]]
 vim.cmd [[ let hidden_statusline = luaeval('require("utils").load_config().ui.hidden_statusline') | autocmd BufEnter,BufWinEnter,WinEnter,CmdwinEnter,TermEnter * nested if index(hidden_statusline, &ft) >= 0 | set laststatus=0 | else | set laststatus=2 | endif ]]
+
+utils.define_augroups {
+   _general_settings = {
+      {
+         "TextYankPost",
+         "*",
+         "lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200})",
+      },
+   },
+}
 
 -- Open a file from its last left off position
 -- vim.cmd [[ au BufReadPost * if expand('%:p') !~# '\m/\.git/' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif ]]
--- lua/utils.lua
+++ lua/utils.lua
@@ -497,4 +497,18 @@ M.update_nvchad = function()
    })
 end
 
+function M.define_augroups(definitions)
+   for group_name, definition in pairs(definitions) do
+      vim.cmd("augroup " .. group_name)
+      vim.cmd "autocmd!"
+
+      for _, def in pairs(definition) do
+         local command = table.concat(vim.tbl_flatten { "autocmd", def }, " ")
+         vim.cmd(command)
+      end
+
+      vim.cmd "augroup END"
+   end
+end
+
 return M
