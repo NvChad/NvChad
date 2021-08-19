47db8afc6e4c8bb476a6bc089d704ffc07e2fb90
Akianonymus <anonymus.aki@gmail.com>
2021-08-20 00:08:45 +0530

    Fix theme switcher statusline reload

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,4 +1,4 @@
-local global_theme = "themes/" .. require("utils").load_config().ui.theme
+local global_theme = "themes/" .. vim.g.nvchad_theme
 local colors = require(global_theme)
 
 local present1, gl = pcall(require, "galaxyline")
