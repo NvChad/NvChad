f7da4c3d92ad290804923279261ad38232e5b1c5
Akianonymus <anonymus.aki@gmail.com>
2021-08-19 11:20:00 +0530

    NvChadUpdater: Make update repo and url configurable | Improve logging

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -38,6 +38,9 @@ M.options = {
    mapleader = " ",
    autosave = false,
    enable_insertNav = true, -- navigation in insertmode
+   -- used for updater
+   update_url = "https://github.com/NvChad/NvChad",
+   update_branch = "main",
 }
 
 -- enable and disable plugins (false for disable)
