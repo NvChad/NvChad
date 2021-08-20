94300d4753bb9b5dcd26604757d572ad4337764d
Akianonymus <anonymus.aki@gmail.com>
2021-08-20 15:23:04 +0530

    dashboard: Fix alignment

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -30,7 +30,7 @@ g.dashboard_custom_section = {
    c = { description = { "  Find Word                 SPC f w" }, command = "Telescope live_grep" },
    d = { description = { "洛 New File                  SPC f n" }, command = "DashboardNewFile" },
    e = { description = { "  Bookmarks                 SPC b m" }, command = "Telescope marks" },
-   f = { description = { "  Load Last Session         SPC l" }, command = "SessionLoad" },
+   f = { description = { "  Load Last Session         SPC l  " }, command = "SessionLoad" },
 }
 
 g.dashboard_custom_footer = {
