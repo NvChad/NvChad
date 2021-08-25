050651c8ff296ae7222a643f7ad8be0edce07d94
Akianonymus <anonymus.aki@gmail.com>
2021-08-25 10:58:56 +0530

    feat: Support for custom user mappings | Improve map function
    move map function to utils
    
    rearrange utils, was missed

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,6 +1,7 @@
 -- IMPORTANT NOTE : This is the user config, can be edited. Will be preserved if updated with internal updater
 
 local M = {}
+M.ui, M.options, M.plugin_status, M.mappings, M.custom = {}, {}, {}, {}, {}
 
 -- non plugin ui configs, available without any plugins
 M.ui = {
@@ -189,6 +190,22 @@ M.mappings.plugin = {
       git = "<leader>gs",
       git_blame = "<leader>gb",
    },
+}
+
+-- user custom mappings
+-- e.g: name = { "mode" , "keys" , "cmd" , "options"}
+-- name: can be empty or something unique with repect to other custom mappings
+--    { mode, key, cmd } or name = { mode, key, cmd }
+-- mode: usage: mode or { mode1, mode2 }, multiple modes allowed, available modes => :h map-modes,
+-- keys: multiple keys allowed, same synxtax as modes
+-- cmd:  for vim commands, must use ':' at start and add <CR> at the end if want to execute
+-- options: see :h nvim_set_keymap() opts section
+M.custom.mappings = {
+   -- clear_all = {
+   --    "n",
+   --    "<leader>cc",
+   --    "gg0vG$d",
+   -- },
 }
 
 return M
