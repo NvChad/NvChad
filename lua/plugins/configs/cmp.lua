475a4c5f38c06537cb708cfafe84261d7b529a58
siduck76 <siduckk76@protonmail.com>
2021-08-27 06:44:58 +0530

    replace compe with cmp

---
+++ lua/plugins/configs/cmp.lua
@@ -0,0 +1,63 @@
+vim.opt.completeopt = "menuone,noselect"
+
+local lspkind = require "lspkind"
+local luasnip = require "luasnip"
+
+-- nvim-cmp setup
+local cmp = require "cmp"
+cmp.setup {
+   snippet = {
+      expand = function(args)
+         require("luasnip").lsp_expand(args.body)
+      end,
+   },
+   formatting = {
+      format = function(entry, vim_item)
+         vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
+
+         vim_item.menu = ({
+            nvim_lsp = "[LSP]",
+            nvim_lua = "[Lua]",
+            buffer = "[BUF]",
+         })[entry.source.name]
+
+         return vim_item
+      end,
+   },
+   mapping = {
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.close(),
+      ["<CR>"] = cmp.mapping.confirm {
+         behavior = cmp.ConfirmBehavior.Replace,
+         select = true,
+      },
+      ["<Tab>"] = function(fallback)
+         if vim.fn.pumvisible() == 1 then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
+         elseif luasnip.expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+         else
+            fallback()
+         end
+      end,
+      ["<S-Tab>"] = function(fallback)
+         if vim.fn.pumvisible() == 1 then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
+         elseif luasnip.jumpable(-1) then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+         else
+            fallback()
+         end
+      end,
+   },
+   sources = {
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+      { name = "buffer" },
+      { name = "nvim_lua" },
+   },
+}
--- lua/plugins/configs/luasnip.lua
+++ lua/plugins/configs/luasnip.lua
@@ -3,64 +3,9 @@ if not present then
    return
 end
 
-local t = function(str)
-   return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-   local col = vim.fn.col "." - 1
-   if col == 0 or vim.fn.getline("."):sub(col, col):match "%s" then
-      return true
-   else
-      return false
-   end
-end
-
-_G.tab_complete = function()
-   if vim.fn.pumvisible() == 1 then
-      return t "<C-n>"
-   elseif luasnip and luasnip.expand_or_jumpable() then
-      return t "<Plug>luasnip-expand-or-jump"
-   elseif check_back_space() then
-      return t "<Tab>"
-   else
-      return vim.fn["compe#complete"]()
-   end
-end
-_G.s_tab_complete = function()
-   if vim.fn.pumvisible() == 1 then
-      return t "<C-p>"
-   elseif luasnip and luasnip.jumpable(-1) then
-      return t "<Plug>luasnip-jump-prev"
-   else
-      return t "<S-Tab>"
-   end
-end
-
-_G.completions = function()
-   local npairs
-   if not pcall(function()
-      npairs = require "nvim-autopairs"
-   end) then
-      return
-   end
-
-   if vim.fn.pumvisible() == 1 then
-      if vim.fn.complete_info()["selected"] ~= -1 then
-         return vim.fn["compe#confirm"] "<CR>"
-      end
-   end
-   return npairs.check_break_line_char()
-end
-
-vim.api.nvim_set_keymap("i", "<CR>", "v:lua.completions()", { expr = true })
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", { expr = true })
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", { expr = true })
-
 luasnip.config.set_config {
    history = true,
    updateevents = "TextChanged,TextChangedI",
 }
+
 require("luasnip/loaders/from_vscode").load()
