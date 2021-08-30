e1dfaa7a453d91dc19daf76ed9ea852f2234aa83
siduck76 <siduckk76@protonmail.com>
2021-08-30 05:54:31 +0530

    add default config for nvim-colorizer

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -56,7 +56,19 @@ end
 M.colorizer = function()
    local present, colorizer = pcall(require, "colorizer")
    if present then
-      colorizer.setup()
+      colorizer.setup({ "*" }, {
+         RGB = true, -- #RGB hex codes
+         RRGGBB = true, -- #RRGGBB hex codes
+         names = false, -- "Name" codes like Blue
+         RRGGBBAA = false, -- #RRGGBBAA hex codes
+         rgb_fn = false, -- CSS rgb() and rgba() functions
+         hsl_fn = false, -- CSS hsl() and hsla() functions
+         css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+         css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+
+         -- Available modes: foreground, background
+         mode = "background", -- Set the display mode.
+      })
       vim.cmd "ColorizerReloadAllBuffers"
    end
 end
