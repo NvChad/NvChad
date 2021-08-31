66d02cf9dd7ea12c522ad4f886e4974ba786638f
Marwan Mustafa <marwan.mm@live.com>
2021-09-01 07:57:56 +1200

    fix(plugin): indentline using config instead of setup

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -43,14 +43,14 @@ M.better_escape = function()
 end
 
 M.blankline = function()
-   vim.g.indentLine_enabled = 1
-   vim.g.indent_blankline_char = "▏"
-
-   vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard", "packer" }
-   vim.g.indent_blankline_buftype_exclude = { "terminal" }
-
-   vim.g.indent_blankline_show_trailing_blankline_indent = false
-   vim.g.indent_blankline_show_first_indent_level = false
+   require("indent_blankline").setup {
+      indentLine_enabled = 1,
+      char = "▏",
+      indent_blankline_filetype_exclude = { "help", "terminal", "dashboard", "packer" },
+      indent_blankline_buftype_exclude = { "terminal" },
+      indent_blankline_show_trailing_blankline_indent = false,
+      indent_blankline_show_first_indent_level = false,
+   }
 end
 
 M.colorizer = function()
