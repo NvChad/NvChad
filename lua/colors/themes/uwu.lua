19e17eac586874e81ec8b9db4902de1576f6b5d4
siduck76 <siduckk76@protonmail.com>
2021-09-01 19:03:34 +0530

    add uwu theme

---
+++ lua/colors/themes/uwu.lua
@@ -0,0 +1,35 @@
+local colors = {
+   white = "#d6d6d6",
+   darker_black = "#0b0f11",
+   black = "#0F1315", -- nvim bg
+   black2 = "#161a1c",
+   one_bg = "#1b1f21",
+   one_bg2 = "#202426",
+   one_bg3 = "#25292b",
+   grey = "#3c4042",
+   grey_fg = "#464a4c",
+   grey_fg2 = "#4a4e50",
+   light_grey = "#515557",
+   red = "#e74c4c",
+   baby_pink = "#FF6E79",
+   pink = "#ef7cbb",
+   line = "#1a1e20", -- for lines like vertsplit
+   green = "#6bb05d",
+   vibrant_green = "#8dc776",
+   nord_blue = "#51a39f",
+   blue = "#66a3b4",
+   yellow = "#f0a972",
+   sun = "#e59e67",
+   purple = "#b4bbc8",
+   dark_purple = "#946fa8",
+   teal = "#5b98a9",
+   orange = "#eaa36c",
+   cyan = "#74b1c2",
+   statusline_bg = "#131719",
+   lightbg = "#2e3238",
+   lightbg2 = "#1d2123",
+   pmenu_bg = "#6bb05d",
+   folder_bg = "#5b98a9",
+}
+
+return colors
