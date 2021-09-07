094ad617850ff81a32fa3f45053152dbd743d8cb
boppyt <71049646+boppyt@users.noreply.github.com>
2021-09-07 15:55:22 -0700

    add jellybeans theme

---
+++ lua/colors/themes/jellybeans.lua
@@ -0,0 +1,35 @@
+local colors = {
+   white = "#e8e8d3",
+   darker_black = "#232323",
+   black = "#151515", --  nvim bg
+   black2 = "#1c1c1c",
+   one_bg = "#353535",
+   one_bg2 = "#3f3f3f",
+   one_bg3 = "#444444",
+   grey = "#464646",
+   grey_fg = "#4e4e4e",
+   grey_fg2 = "#505050",
+   light_grey = "#565656",
+   red = "#cf6a4c",
+   baby_pink = "#f6c6d9",
+   pink = "#f0a0c0",
+   line = "#605958", -- for lines like vertsplit
+   green = "#99ad6a",
+   vibrant_green = "#c2cea6",
+   nord_blue = "#bcd9ea",
+   blue = "#8fbfdc",
+   yellow = "#fad07a",
+   sun = "#ffb964",
+   purple = "#c6b6ee",
+   dark_purple = "#9e83e7",
+   teal = "#668799",
+   orange = "#e78a4e",
+   cyan = "#8197bf",
+   statusline_bg = "#2c2c2c",
+   lightbg = "#353535",
+   lightbg2 = "#303030",
+   pmenu_bg = "#8197bf",
+   folder_bg = "#8197bf",
+}
+
+return colors
