4ac5a2cf7702fa060a9b235c7bb333b2489c42b8
Galen Rowell <growell3@gmail.com>
2021-09-24 18:03:33 +1000

    feat: improve mappings, move `custom/init.lua & chadrc.lua` -> `custom/example_init.lua & example_chadrc.lua`

--- lua/custom/example_chadrc.lua
+++ lua/custom/example_chadrc.lua
@@ -1,31 +1,23 @@
 -- IMPORTANT NOTE : This is the user config, can be edited. Will be preserved if updated with internal updater
+-- This file is for NvChad options & tools, custom settings are split between here and 'lua/custom/init.lua'
 
 local M = {}
 M.options, M.ui, M.mappings, M.plugins = {}, {}, {}, {}
 
+-- NOTE: To use this, make a copy with `cp example_chadrc.lua chadrc.lua`
+
+--------------------------------------------------------------------
+
 -- To use this file, copy the strucutre of `core/default_config.lua`,
--- then define your new var, an example of setting relative number:
+-- examples of setting relative number & changing theme:
 
 -- M.options = {
 --    relativenumber = true,
 -- }
 
-
--- user custom mappings
--- format: name = { "mode" , "keys" , "cmd" , "options"}
-  -- name: can be empty or something unique with repect to other custom mappings
-  --    { mode, key, cmd } or name = { mode, key, cmd }
-  -- mode: usage: mode or { mode1, mode2 }, multiple modes allowed, available modes => :h map-modes,
-  -- keys: multiple keys allowed, same synxtax as modes
-  -- cmd:  for vim commands, must use ':' at start and add <CR> at the end if want to execute
-  -- options: see :h nvim_set_keymap() opts section
-M.mappings.custom = {
-   -- clear_all = {
-   --    "n",
-   --    "<leader>cc",
-   --    "gg0vG$d",
-   -- },
-}
+-- M.ui = {
+--   theme = "nord"
+-- }
 
 -- NvChad included plugin options & overrides
 M.plugins = {
