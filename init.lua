8726de1faa9cc121b35876744ca2d01eeb0f06d7
Ashin Antony <83629316+ashincoder@users.noreply.github.com>
2021-09-28 14:53:26 +0530

    chore: Clean init.lua
    Why do we have a table to require a single module. We had this when we had plugins, colors etc. 
    But we only have one module now. So why bother a list and looping through for a single module ?

--- init.lua
+++ init.lua
@@ -1,10 +1,4 @@
-local init_modules = {
-   "core",
-}
-
-for _, module in ipairs(init_modules) do
-   local ok, err = pcall(require, module)
-   if not ok then
-      error("Error loading " .. module .. "\n\n" .. err)
-   end
+local ok, err = pcall(require, "core")
+if not ok then
+	error("Error loading " .. module .. "\n\n" .. err)
 end
