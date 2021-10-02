072d6f7482ee5022f3866c4a95ab1afa3174d2cf
Akianonymus <anonymus.aki@gmail.com>
2021-10-02 10:59:23 +0530

    fix: Packer blank window on PackerSync

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -120,7 +120,7 @@ return packer.startup(function()
          require("core.utils").packer_lazy_load "nvim-lspconfig"
          -- reload the current file so lsp actually starts for it
          vim.defer_fn(function()
-            vim.cmd "silent! e %"
+            vim.cmd 'if &ft == "packer" | echo "" | else | silent! e %'
          end, 0)
       end,
       config = override_req("lspconfig", "plugins.configs.lspconfig"),
