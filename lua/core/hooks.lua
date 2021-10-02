6b116afa9337ec1dc03efc9425ff941820fb4764
Akianonymus <anonymus.aki@gmail.com>
2021-10-02 10:45:50 +0530

    chore: format files

--- lua/core/hooks.lua
+++ lua/core/hooks.lua
@@ -1,69 +1,68 @@
-local hooks, overrides, M = {}, {}, {};
+local hooks, overrides, M = {}, {}, {}
 local allowed_hooks = {
-  "install_plugins",
-  "setup_mappings",
-  "ready",
+   "install_plugins",
+   "setup_mappings",
+   "ready",
 }
 
-local function has_value (tab, val)
-  for _, value in ipairs(tab) do
-    if value == val then
-      return true
-    end
-  end
+local function has_value(tab, val)
+   for _, value in ipairs(tab) do
+      if value == val then
+         return true
+      end
+   end
 
-  return false
+   return false
 end
 
 M.add = function(name, fn)
-  if not(has_value(allowed_hooks, name)) then
-    error("Custom lua uses unallowed hook " .. name)
-  end
-  if hooks[name] == nil then
-    hooks[name] = {}
-  end
-  table.insert(hooks[name], fn);
+   if not (has_value(allowed_hooks, name)) then
+      error("Custom lua uses unallowed hook " .. name)
+   end
+   if hooks[name] == nil then
+      hooks[name] = {}
+   end
+   table.insert(hooks[name], fn)
 end
 
 M.run = function(name, args)
-  if hooks[name] == nil then
-    return;
-  end
+   if hooks[name] == nil then
+      return
+   end
 
-  for _, hook in pairs(hooks[name]) do
-    hook(args)
-  end
+   for _, hook in pairs(hooks[name]) do
+      hook(args)
+   end
 end
 
 M.createOverrides = function(module)
-  local O = {};
+   local O = {}
 
-  O.get = function(name, default)
-    local current = default;
-    if overrides[module] and overrides[module][name] then
-      if type(overrides[module][name]) == "function" then
-         current = overrides[module][name]
-      elseif type(overrides[module][name]) == "table" then
-         for _, override in pairs(overrides[module][name]) do
-           current = override(current)
+   O.get = function(name, default)
+      local current = default
+      if overrides[module] and overrides[module][name] then
+         if type(overrides[module][name]) == "function" then
+            current = overrides[module][name]
+         elseif type(overrides[module][name]) == "table" then
+            for _, override in pairs(overrides[module][name]) do
+               current = override(current)
+            end
          end
       end
-    end
-    return current;
-  end
+      return current
+   end
 
-  return O;
+   return O
 end
 
 M.override = function(module, name, overwrite)
-  if overrides[module] == nil then
-    overrides[module] = {};
-  end
-  if overrides[module][name] == nil then
-    overrides[module][name] = {};
-  end
-  table.insert(overrides[module][name], overwrite)
+   if overrides[module] == nil then
+      overrides[module] = {}
+   end
+   if overrides[module][name] == nil then
+      overrides[module][name] = {}
+   end
+   table.insert(overrides[module][name], overwrite)
 end
 
-
-return M;
+return M
