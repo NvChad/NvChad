9fceac0b6623f40ea306411432ed4edd1399ae08
max397574 <max.heinzer397@gmx.ch>
2021-10-08 15:36:15 +0200

    fixed formatting

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -44,7 +44,7 @@ cmp.setup {
       },
       ["<Tab>"] = function(fallback)
          if cmp.visible() then
-			cmp.select_next_item()
+            cmp.select_next_item()
          elseif require("luasnip").expand_or_jumpable() then
             vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
          else
@@ -52,9 +52,9 @@ cmp.setup {
          end
       end,
       ["<S-Tab>"] = function(fallback)
-		if cmp.visible() then
-			cmp.select_prev_item()
-		elseif require("luasnip").jumpable(-1) then
+         if cmp.visible() then
+            cmp.select_prev_item()
+         elseif require("luasnip").jumpable(-1) then
             vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
          else
             fallback()
