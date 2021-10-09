133a1fdb781efc02110d76fa85d9c4ce24e50d28
thuan1412 <36019052+thuan1412@users.noreply.github.com>
2021-10-09 16:18:09 +0700

    update nvim-cmp plugin
    follow the breaking change of `nvim-cmp`

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -43,22 +43,18 @@ cmp.setup {
          select = true,
       },
       ["<Tab>"] = function(fallback)
-         if cmp.visible() then
-            cmp.select_next_item()
-         elseif require("luasnip").expand_or_jumpable() then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-         else
-            fallback()
-         end
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
       end,
       ["<S-Tab>"] = function(fallback)
-         if cmp.visible() then
-            cmp.select_prev_item()
-         elseif require("luasnip").jumpable(-1) then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-         else
-            fallback()
-         end
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
       end,
    },
    sources = {
