6c10e76070109baeddfc307b7c30a4c536c364aa
Shuduo Sang <sangshuduo@gmail.com>
2021-10-13 17:23:04 +0800

    add listchars

--- init.lua
+++ init.lua
@@ -3,3 +3,14 @@ local ok, err = pcall(require, "core")
 if not ok then
    error("Error loading core" .. "\n\n" .. err)
 end
+
+-- tab:→\ ,trail:␣,extends:…,eol:⏎
+-- tab:>- ,trail:~,extends:…,eol:↵
+vim.opt.list = true
+vim.opt.listchars:append("eol:⏎")
+vim.opt.listchars:append("space: ")
+vim.opt.listchars:append("trail:␣")
+vim.opt.listchars:append("extends:…")
+vim.opt.listchars:append("nbsp:␣")
+vim.opt.listchars:append("tab:>-")
+
