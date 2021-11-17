07b4d19b8fa13ec6093ebd8e2626e58ffcf70a60
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2021-11-17 10:04:37 -0500

    feat(nvimtree): exclude terminal from window picker

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -17,6 +17,11 @@ g.nvim_tree_indent_markers = 1
 g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
+g.nvim_tree_window_picker_exclude = {
+   filetype = { 'notify', 'packer', 'qf' },
+   buftype = {'terminal' },
+}
+
 --
 g.nvim_tree_show_icons = {
    folders = 1,
