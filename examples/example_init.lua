b4da4901382aebb948ff018d63f7206897287643
siduck <siduck@tutanota.com>
2021-11-19 07:44:06 +0530

    gitignore custom dir | add example configs

---
+++ examples/example_init.lua
@@ -0,0 +1,39 @@
+-- This is an example init file , its supposed to be placed in /lua/custom dir
+-- lua/custom/init.lua
+
+-- This is where your custom modules and plugins go.
+-- Please check NvChad docs if you're totally new to nvchad + dont know lua!!
+
+local hooks = require "core.hooks"
+
+-- MAPPINGS
+-- To add new plugins, use the "setup_mappings" hook,
+
+hooks.add("setup_mappings", function(map)
+   map("n", "<leader>cc", ":Telescope <CR>", opt)
+   map("n", "<leader>q", ":q <CR>", opt)
+end)
+
+-- NOTE : opt is a variable  there (most likely a table if you want multiple options),
+-- you can remove it if you dont have any custom options
+
+-- Install plugins
+-- To add new plugins, use the "install_plugin" hook,
+
+-- examples below:
+
+hooks.add("install_plugins", function(use)
+   use {
+      "max397574/better-escape.nvim",
+      event = "InsertEnter",
+   }
+
+   use {
+      "user or orgname/reponame",
+      --further packer options
+   }
+end)
+
+-- NOTE: we heavily suggest using Packer's lazy loading (with the 'event' field)
+-- see: https://github.com/wbthomason/packer.nvim
+-- https://nvchad.github.io/config/walkthrough
--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -9,7 +9,6 @@ local g = vim.g
 
 g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
 g.nvim_tree_git_hl = git_status
-g.nvim_tree_gitignore = 0
 g.nvim_tree_highlight_opened_files = 0
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
