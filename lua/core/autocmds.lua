1758f5654156ddb785825bc7e2738d459a383680
Akianonymus <anonymus.aki@gmail.com>
2021-12-26 10:09:19 +0530

    statusline: Refresh after BufRead too
    sometimes events like BufEnter, BufWinEnter, etc are not enough for triggering a refresh, like opening nvim when nvimtree is not lazy load

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -9,7 +9,7 @@
 vim.cmd [[ au TermOpen term://* setlocal nonumber norelativenumber | setfiletype terminal ]]
 
 -- Don't show status line on certain windows
-vim.cmd [[ autocmd BufEnter,BufWinEnter,FileType,WinEnter * lua require("core.utils").hide_statusline() ]]
+vim.cmd [[ autocmd BufEnter,BufRead,BufWinEnter,FileType,WinEnter * lua require("core.utils").hide_statusline() ]]
 
 -- Open a file from its last left off position
 -- vim.cmd [[ au BufReadPost * if expand('%:p') !~# '\m/\.git/' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif ]]
