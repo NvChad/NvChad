555cd5a0e720d6c0a15ebe2e8cfea2e84a119e83
Danilo Horta <danilo.horta@pm.me>
2022-01-11 22:59:35 +0000

    Enable FastWrap capabilities: https://github.com/windwp/nvim-autopairs#fastwrap

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -7,7 +7,7 @@ M.autopairs = function()
    local present2, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
 
    if present1 and present2 then
-      autopairs.setup()
+      autopairs.setup({fast_wrap = {}})
 
       local cmp = require "cmp"
       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
