ffb8e604059abc49f62fa6e8ac01ddd050512bcc
i3cheese <i3cheese@yandex.ru>
2022-01-30 22:39:47 +0700

    Toggle relative numbers and copy to system clipboards mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -84,9 +84,12 @@ M.misc = function()
       map("n", maps.misc.cheatsheet, ":lua require('nvchad.cheatsheet').show() <CR>") -- show keybinds
       map("n", maps.misc.close_buffer, ":lua require('core.utils').close_buffer() <CR>") -- close  buffer
       map("n", maps.misc.copy_whole_file, ":%y+ <CR>") -- copy whole file content
+      map("v", maps.misc.copy_to_system_clipboard, "\"+y") 
+      map("n", maps.misc.copy_to_system_clipboard, "\"+yy") -- copy curent line in normal mode
       map("n", maps.misc.new_buffer, ":enew <CR>") -- new buffer
       map("n", maps.misc.new_tab, ":tabnew <CR>") -- new tabs
       map("n", maps.misc.line_number_toggle, ":set nu! <CR>") -- toggle numbers
+      map("n", maps.misc.relative_line_number_toggle, ":set rnu! <CR>") -- toggle relative numbers
       map("n", maps.misc.save_file, ":w <CR>") -- ctrl + s to save file
 
       -- terminal mappings --
