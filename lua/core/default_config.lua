f4c4d8f6c6f86be7b4beeabcb4e620dbae7dfd9c
siduck <siduck@tutanota.com>
2022-02-04 22:17:10 +0530

    enable snippets by default

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -69,7 +69,7 @@ M.plugins = {
       lspsignature = true, -- lsp enhancements
       vim_matchup = true, -- improved matchit
       cmp = true,
-      snippets = false,
+      snippets = true,
       nvimtree = true,
       autopairs = true,
    },
