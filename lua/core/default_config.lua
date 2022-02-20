c6d36a7ff3b2db32cd15093874e6cedfa80649b6
siduck <siduck@tutanota.com>
2022-02-20 08:07:54 +0530

    Update default_config.lua

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -34,7 +34,7 @@ M.options = {
       copy_del = true, -- copy deleted text ( dd key ), visual and normal mode
       insert_nav = true, -- navigation in insertmode
       window_nav = true,
-      terminal_numbers = false,
+      terminal_linenr = true,
 
       -- updater
       update_url = "https://github.com/NvChad/NvChad",
