e1cdf92a01c6fe88e70abde2f0d7d3144262038d
siduck <siduck@tutanota.com>
2022-03-02 14:43:29 +0530

    clean nvimtree config
    recently nvimtree had some updates regarding these options moving into the setup table of its config and these were the default ones so i removed it!

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -10,13 +10,7 @@ g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
 g.nvim_tree_git_hl = 0
 g.nvim_tree_highlight_opened_files = 0
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
-
-g.nvim_tree_window_picker_exclude = {
-   filetype = { "notify", "packer", "qf" },
-   buftype = { "terminal" },
-}
 
 g.nvim_tree_show_icons = {
    folders = 1,
