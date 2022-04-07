c2c02934b7bdae180be28c297dd0ab8f8904b2a1
Arman.H <ahhdev@gmail.com>
2022-04-07 12:18:13 +0430

    refactor: rewrite autocommands with lua

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,20 +1,51 @@
-local settings=require("core.utils").load_config().options.nvChad
--- uncomment this if you want to open nvim with a dir
--- vim.cmd [[ autocmd BufEnter * if &buftype != "terminal" | lcd %:p:h | endif ]]
+local settings = require("core.utils").load_config().options.nvChad
+
+-- Uncomment this if you want to open nvim with a dir
+-- vim.api.nvim_create_autocmd("BufEnter", { command = [[if &buftype != "terminal" | lcd %:p:h | endif]] })
 
 -- Use relative & absolute line numbers in 'n' & 'i' modes respectively
--- vim.cmd[[ au InsertEnter * set norelativenumber ]]
--- vim.cmd[[ au InsertLeave * set relativenumber ]]
+-- vim.api.nvim_create_autocmd("InsertEnter", {
+--    callback = function()
+--       vim.opt.relativenumber = false
+--    end,
+-- })
+-- vim.api.nvim_create_autocmd("InsertLeave", {
+--    callback = function()
+--       vim.opt.relativenumber = true
+--    end,
+-- })
 
 -- Don't show any numbers inside terminals
 if not settings.terminal_numbers then
-   vim.cmd [[ au TermOpen term://* setlocal nonumber norelativenumber | setfiletype terminal ]]
+   vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "term://*",
+      callback = function()
+         vim.opt_local.number = false
+         vim.opt_local.relativenumber = false
+         vim.cmd [[ setfiletype terminal ]]
+      end,
+   })
 end
 
 -- Don't show status line on certain windows
-vim.cmd [[ autocmd BufEnter,BufRead,BufWinEnter,FileType,WinEnter * lua require("core.utils").hide_statusline() ]]
+vim.api.nvim_create_autocmd({ "BufEnter", "BufRead", "BufWinEnter", "FileType", "WinEnter" }, {
+   callback = function()
+      require("core.utils").hide_statusline()
+   end,
+})
 
 -- Open a file from its last left off position
--- vim.cmd [[ au BufReadPost * if expand('%:p') !~# '\m/\.git/' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif ]]
+-- vim.api.nvim_create_autocmd("BufReadPost", {
+--    command = [[ if expand('%:p') !~# '\m/\.git/' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif ]],
+-- })
+
 -- File extension specific tabbing
--- vim.cmd [[ autocmd Filetype python setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4 ]]
+-- vim.api.nvim_create_autocmd("Filetype", {
+--    pattern = "python",
+--    callback = function()
+--       vim.opt_local.expandtab = true
+--       vim.opt_local.tabstop = 4
+--       vim.opt_local.shiftwidth = 4
+--       vim.opt_local.softtabstop = 4
+--    end,
+-- })
