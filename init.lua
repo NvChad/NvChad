983529cca89c739eb5e8a984756a88bf5a02cb02
siduck <siduck@tutanota.com>
2022-04-22 07:28:17 +0530

    rm un-needed check for custom config as we dont need it!

--- init.lua
+++ init.lua
@@ -16,13 +16,3 @@ for _, module in ipairs(core_modules) do
       error("Error loading " .. module .. "\n\n" .. err)
    end
 end
-
--- check if custom init.lua file exists
-if vim.fn.filereadable(vim.fn.stdpath "config" .. "/lua/custom/init.lua") == 1 then
-   -- try to call custom init, if not successful, show error
-   local ok, err = pcall(require, "custom")
-   if not ok then
-      vim.notify("Error loading custom/init.lua\n\n" .. err)
-   end
-   return
-end
