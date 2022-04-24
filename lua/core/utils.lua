ee0fbae116395a503876fe6a8ea2ab9468c57eaf
siduck <siduck@tutanota.com>
2022-04-24 20:14:03 +0530

    tmp

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -136,52 +136,19 @@ end
 
 M.map = function(mode, keys, command, opt)
    local options = { noremap = true, silent = true }
+
    if opt then
       options = vim.tbl_extend("force", options, opt)
    end
 
-   -- all valid modes allowed for mappings
-   -- :h map-modes
-   local valid_modes = {
-      [""] = true,
-      ["n"] = true,
-      ["v"] = true,
-      ["s"] = true,
-      ["x"] = true,
-      ["o"] = true,
-      ["!"] = true,
-      ["i"] = true,
-      ["l"] = true,
-      ["c"] = true,
-      ["t"] = true,
-   }
-
-   -- helper function for M.map
-   -- can gives multiple modes and keys
-   local function map_wrapper(sub_mode, lhs, rhs, sub_options)
-      if type(lhs) == "table" then
-         for _, key in ipairs(lhs) do
-            map_wrapper(sub_mode, key, rhs, sub_options)
-         end
-      else
-         if type(sub_mode) == "table" then
-            for _, m in ipairs(sub_mode) do
-               map_wrapper(m, lhs, rhs, sub_options)
-            end
-         else
-            if valid_modes[sub_mode] and lhs and rhs then
-               vim.api.nvim_set_keymap(sub_mode, lhs, rhs, sub_options)
-            else
-               sub_mode, lhs, rhs = sub_mode or "", lhs or "", rhs or ""
-               print(
-                  "Cannot set mapping [ mode = '" .. sub_mode .. "' | key = '" .. lhs .. "' | cmd = '" .. rhs .. "' ]"
-               )
-            end
-         end
+   if type(keys) == "table" then
+      for _, keymap in ipairs(keys) do
+         M.map(mode, keymap, command, opt)
       end
+      return
    end
 
-   map_wrapper(mode, keys, command, options)
+   vim.keymap.set(mode, keys, command, opt)
 end
 
 -- load plugin after entering vim ui
