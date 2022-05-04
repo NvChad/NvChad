06d930efece22267461ac325286586d42b54a7f8
Arman.H <ahhdev@gmail.com>
2022-05-04 09:55:04 +0430

    feat: add more autocommands

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -41,3 +41,18 @@ local autocmd = vim.api.nvim_create_autocmd
 --       vim.opt_local.softtabstop = 4
 --    end,
 -- })
+
+-- Highlight yanked text
+-- autocmd("TextYankPost", {
+--    callback = function()
+--       vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+--    end,
+-- })
+
+-- Enable spellchecking in markdown, text and gitcommit files
+-- autocmd("FileType", {
+--    pattern = { "gitcommit", "markdown", "text" },
+--    callback = function()
+--       vim.opt_local.spell = true
+--    end,
+-- })
