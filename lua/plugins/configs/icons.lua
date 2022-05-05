9472212ceef3ebc8a44a17e6152a6ba1b4b7fc3c
Akianonymus <anonymus.aki@gmail.com>
2022-05-05 13:43:53 +0530

    Format files

--- lua/plugins/configs/icons.lua
+++ lua/plugins/configs/icons.lua
@@ -4,7 +4,7 @@ if not present then
    return
 end
 
-local colors = require("base16").get_colors("base_30")
+local colors = require("base16").get_colors "base_30"
 
 local options = {
    override = {
