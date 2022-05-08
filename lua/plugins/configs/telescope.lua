f235c3d12283a5a7c7ea70a82c900200b6223081
Akianonymus <anonymus.aki@gmail.com>
2022-05-08 21:47:27 +0530

    telescope: Add mapping to close picker with q in normal mode

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -50,6 +50,9 @@ local options = {
       qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
       -- Developer configurations: Not meant for general override
       buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+      mappings = {
+         n = { ["q"] = require("telescope.actions").close },
+      },
    },
 }
 
