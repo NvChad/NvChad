771c086f97f0534d8dea39855df482f89fc3438f
lixvbnet <lixvbnet@gmail.com>
2022-05-16 20:38:26 +0800

    run './build.sh release' to tag all plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,17 +4,27 @@ if not present then
    return false
 end
 
+local V = require "plugins.versions"
+
 local plugins = {
-   ["nvim-lua/plenary.nvim"] = {},
-   ["lewis6991/impatient.nvim"] = {},
+   ["nvim-lua/plenary.nvim"] = {
+      commit = V.plenary_nvim,
+   },
+   ["lewis6991/impatient.nvim"] = {
+      commit = V.impatient_nvim,
+   },
 
    ["wbthomason/packer.nvim"] = {
+      commit = V.packer_nvim,
       event = "VimEnter",
    },
 
-   ["NvChad/extensions"] = {},
+   ["NvChad/extensions"] = {
+      commit = V.extensions,
+   },
 
    ["NvChad/base46"] = {
+      commit = V.base46,
       after = "packer.nvim",
       config = function()
          local ok, base46 = pcall(require, "base46")
@@ -26,12 +36,14 @@ local plugins = {
    },
 
    ["NvChad/nvterm"] = {
+      commit = V.nvterm,
       config = function()
          require "plugins.configs.nvterm"
       end,
    },
 
    ["kyazdani42/nvim-web-devicons"] = {
+      commit = V.nvim_web_devicons,
       after = "base46",
       config = function()
          require "plugins.configs.icons"
@@ -39,6 +51,7 @@ local plugins = {
    },
 
    ["feline-nvim/feline.nvim"] = {
+      commit = feline_nvim,
       after = "nvim-web-devicons",
       config = function()
          require "plugins.configs.statusline"
@@ -46,6 +59,7 @@ local plugins = {
    },
 
    ["akinsho/bufferline.nvim"] = {
+      commit = V.bufferline_nvim,
       after = "nvim-web-devicons",
       config = function()
          require "plugins.configs.bufferline"
@@ -53,6 +67,7 @@ local plugins = {
    },
 
    ["lukas-reineke/indent-blankline.nvim"] = {
+      commit = V.indent_blankline_nvim,
       event = "BufRead",
       config = function()
          require("plugins.configs.others").blankline()
@@ -60,6 +75,7 @@ local plugins = {
    },
 
    ["NvChad/nvim-colorizer.lua"] = {
+      commit = V.nvim_colorizer_lua,
       event = "BufRead",
       config = function()
          require("plugins.configs.others").colorizer()
@@ -67,6 +83,7 @@ local plugins = {
    },
 
    ["nvim-treesitter/nvim-treesitter"] = {
+      commit = V.nvim_treesitter,
       event = { "BufRead", "BufNewFile" },
       run = ":TSUpdate",
       config = function()
@@ -76,6 +93,7 @@ local plugins = {
 
    -- git stuff
    ["lewis6991/gitsigns.nvim"] = {
+      commit = V.gitsigns_nvim,
       opt = true,
       config = function()
          require("plugins.configs.others").gitsigns()
@@ -88,6 +106,7 @@ local plugins = {
    -- lsp stuff
 
    ["williamboman/nvim-lsp-installer"] = {
+      commit = V.nvim_lsp_installer,
       opt = true,
       setup = function()
          nvchad.packer_lazy_load "nvim-lsp-installer"
@@ -99,6 +118,7 @@ local plugins = {
    },
 
    ["neovim/nvim-lspconfig"] = {
+      commit = V.nvim_lspconfig,
       after = "nvim-lsp-installer",
       module = "lspconfig",
       config = function()
@@ -108,6 +128,7 @@ local plugins = {
    },
 
    ["ray-x/lsp_signature.nvim"] = {
+      commit = V.lsp_signature_nvim,
       after = "nvim-lspconfig",
       config = function()
          require("plugins.configs.others").signature()
@@ -115,6 +136,7 @@ local plugins = {
    },
 
    ["andymass/vim-matchup"] = {
+      commit = V.vim_matchup,
       opt = true,
       setup = function()
          nvchad.packer_lazy_load "vim-matchup"
@@ -122,6 +144,7 @@ local plugins = {
    },
 
    ["max397574/better-escape.nvim"] = {
+      commit = V.better_escape_nvim,
       event = "InsertCharPre",
       config = function()
          require("plugins.configs.others").better_escape()
@@ -131,11 +154,13 @@ local plugins = {
    -- load luasnips + cmp related in insert mode only
 
    ["rafamadriz/friendly-snippets"] = {
+      commit = V.friendly_snippets,
       module = "cmp_nvim_lsp",
       event = "InsertEnter",
    },
 
    ["hrsh7th/nvim-cmp"] = {
+      commit = V.nvim_cmp,
       after = "friendly-snippets",
       config = function()
          require "plugins.configs.cmp"
@@ -143,6 +168,7 @@ local plugins = {
    },
 
    ["L3MON4D3/LuaSnip"] = {
+      commit = V.luasnip,
       wants = "friendly-snippets",
       after = "nvim-cmp",
       config = function()
@@ -151,27 +177,33 @@ local plugins = {
    },
 
    ["saadparwaiz1/cmp_luasnip"] = {
+      commit = V.cmp_luasnip,
       after = "LuaSnip",
    },
 
    ["hrsh7th/cmp-nvim-lua"] = {
+      commit = V.cmp_nvim_lua,
       after = "cmp_luasnip",
    },
 
    ["hrsh7th/cmp-nvim-lsp"] = {
+      commit = V.cmp_nvim_lsp,
       after = "cmp-nvim-lua",
    },
 
    ["hrsh7th/cmp-buffer"] = {
+      commit = V.cmp_buffer,
       after = "cmp-nvim-lsp",
    },
 
    ["hrsh7th/cmp-path"] = {
+      commit = V.cmp_path,
       after = "cmp-buffer",
    },
 
    -- misc plugins
    ["windwp/nvim-autopairs"] = {
+      commit = V.nvim_autopairs,
       after = "nvim-cmp",
       config = function()
          require("plugins.configs.others").autopairs()
@@ -179,6 +211,7 @@ local plugins = {
    },
 
    ["goolord/alpha-nvim"] = {
+      -- commit = V.alpha_nvim,     -- can't config disabled plugin
       disable = true,
       config = function()
          require "plugins.configs.alpha"
@@ -186,6 +219,7 @@ local plugins = {
    },
 
    ["numToStr/Comment.nvim"] = {
+      commit = V.Comment_nvim,
       module = "Comment",
       keys = { "gc", "gb" },
       config = function()
@@ -195,6 +229,7 @@ local plugins = {
 
    -- file managing , picker etc
    ["kyazdani42/nvim-tree.lua"] = {
+      commit = V.nvim_tree_lua,
       cmd = { "NvimTreeToggle", "NvimTreeFocus" },
       config = function()
          require "plugins.configs.nvimtree"
@@ -202,6 +237,7 @@ local plugins = {
    },
 
    ["nvim-telescope/telescope.nvim"] = {
+      commit = V.telescope_nvim,
       cmd = "Telescope",
       config = function()
          require "plugins.configs.telescope"
