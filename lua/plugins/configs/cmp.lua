89aea0b623275c8f648783431bcca781823c8389
Arman.H <ahhdev@gmail.com>
2022-05-16 17:39:22 +0430

    fix(cmp): change the confirmation behaviour to `Insert`

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -55,7 +55,7 @@ local options = {
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.close(),
       ["<CR>"] = cmp.mapping.confirm {
-         behavior = cmp.ConfirmBehavior.Replace,
+         behavior = cmp.ConfirmBehavior.Insert,
          select = true,
       },
       ["<Tab>"] = cmp.mapping(function(fallback)
