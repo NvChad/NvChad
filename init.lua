ed9082dfb4bb66243ebcf698d32c5062556cbfeb
siduck <siduck@tutanota.com>
2022-05-23 14:54:59 +0530

    load custom module | fix https://github.com/NvChad/NvChad/commit/a2ff5c285e3fe96026db2852f169a6703090e26e#commitcomment-74274032

--- init.lua
+++ init.lua
@@ -11,3 +11,9 @@ require "core.options"
 -- setup packer + plugins
 require("core.packer").bootstrap()
 require "plugins"
+
+local user_conf, _ = pcall(require, "custom")
+
+if user_conf then
+   require "custom"
+end
