e50f937429122d9aac0937f7ef9a5e22699bf2d3
Anonymous1231-dev <87801676+Anonymous1231-dev@users.noreply.github.com>
2022-05-29 12:46:31 +0800

    chore: Add some beautiful icons
    Signed-off-by: Anonymous1231-dev <87801676+Anonymous1231-dev@users.noreply.github.com>

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -25,10 +25,10 @@ M.options = {
    compile_on_sync = true,
    git = { clone_timeout = 6000 },
    display = {
-      working_sym = "ﲊ",
-      error_sym = "✗",
-      done_sym = "﫟",
-      removed_sym = "",
+      working_sym = " ﲊ",
+      error_sym = "✗ ",
+      done_sym = " ",
+      removed_sym = " ",
       moved_sym = "",
       open_fn = function()
          return require("packer.util").float { border = "single" }
