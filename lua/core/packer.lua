34fbb2e06626b1b68ac45b11464d348a04316391
siduck <siduck@tutanota.com>
2022-05-29 19:26:12 +0530

    Revert "fix #1141"
    This reverts commit c5bcf9d9a17ccb278138e8647343542b20cb5ae6.

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -2,13 +2,11 @@ local M = {}
 
 M.bootstrap = function()
    local fn = vim.fn
-   local base_path = fn.stdpath "data" .. "/site/pack/packer"
-   local opt, start = base_path .. "/opt/packer.nvim", base_path .. "/start/packer.nvim"
-   local install_path = fn.isdirectory(opt) and opt or start
+   local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 
    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
 
-   if not fn.isdirectory(install_path) then
+   if fn.empty(fn.glob(install_path)) > 0 then
       print "Cloning packer .."
 
       fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
