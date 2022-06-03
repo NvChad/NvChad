f1213c0c3c965209f611745fede1863df6a6617a
Mohammad Hosein <forghani77@gmail.com>
2022-06-03 05:42:06 +0430

    change proiority of completion list in nvim-cmp

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -87,8 +87,8 @@ local options = {
       }),
    },
    sources = {
-      { name = "nvim_lsp" },
       { name = "luasnip" },
+      { name = "nvim_lsp" },
       { name = "buffer" },
       { name = "nvim_lua" },
       { name = "path" },
