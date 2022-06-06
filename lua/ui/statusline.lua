d9263b3718dca8fdd9fdaefa98ee63d6fd608733
siduck <siduck@tutanota.com>
2022-06-06 10:10:35 +0530

    add error handling in statusline for devicons plugin

--- lua/ui/statusline.lua
+++ lua/ui/statusline.lua
@@ -71,7 +71,13 @@ M.fileInfo = function()
       filename = " " .. filename .. " "
    end
 
-   local ft_icon = require("nvim-web-devicons").get_icon(filename, extension)
+   local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+
+   if not devicons_present then
+      return " "
+   end
+
+   local ft_icon = devicons.get_icon(filename, extension)
    icon = (ft_icon ~= nil and " " .. ft_icon) or icon
 
    return "%#St_file_info#" .. icon .. filename .. "%#St_file_sep#" .. sep_r
