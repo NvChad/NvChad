d327582dc033759e7a5c4a0046d88a168e1da97a
Sabu Siyad <hello@ssiyad.com>
2022-06-22 13:08:13 +0530

    telescope: remove deprecated option: `use_less`

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -47,7 +47,6 @@ local options = {
       border = {},
       borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
       color_devicons = true,
-      use_less = true,
       set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
       file_previewer = require("telescope.previewers").vim_buffer_cat.new,
       grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
