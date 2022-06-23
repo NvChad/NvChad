527f474b145501d7e8438742cd7588ec30a6da50
mehdi <mehdi@pop-os.localdomain>
2022-06-23 15:17:12 +0430

    feat: indent_blankline use treesitter

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -11,11 +11,11 @@ opt.expandtab = true
 opt.tabstop = 4
 opt.shiftwidth = 4
 
+-- [[ indent-blankine plugin]]
+opt.list = true
+g.indent_blankline_use_treesitter = true
 
--- indent_blankline plugin
--- opt.list = true
--- g.indent_blankline_use_treesitter = true
-
--- sneak vim plugin
+-- [[ vim-sneak plugin ]]
 g["sneak#label"] = 1
+-- O, o, I, i cause problem in labels because we are in normal mode
 g["sneak#target_labels"] = ";sftunq/SFGHLTUNRMQZ"
