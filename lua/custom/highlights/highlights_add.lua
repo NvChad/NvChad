cc71a6cc808a1926521d571c52dbb71ad7e1525d
mehdi <mehdi@pop-os.localdomain>
2022-06-23 20:54:23 +0430

    fix: NvimTreeGitStaged color must be added ,not override

--- lua/custom/highlights/highlights_add.lua
+++ lua/custom/highlights/highlights_add.lua
@@ -1,5 +1,6 @@
 return {
-    Sneak = {fg = "#000000", bg = "#95a5a6"}
+    Sneak = {fg = "#000000", bg = "#95a5a6"},
+    NvimTreeGitStaged = {fg = "#ffeaa7"}
 
     -- NvimTreeGitDirty
     -- NvimTreeGitStaged
--- lua/custom/highlights/highlights_override.lua
+++ lua/custom/highlights/highlights_override.lua
@@ -2,7 +2,7 @@ return {
     -- https://flatuicolors.com/palette/us
     NvimTreeGitNew = {fg = "#55efc4"},
     NvimTreeGitDirty = {fg = "#fd79a8"},
-    NvimTreeGitStaged = {fg = "#ffeaa7"},
+    -- NvimTreeGitStaged = {fg = "#ffeaa7"},
     NvimTreeGitMerge = {fg = "#74b9ff"},
     NvimTreeGitDeleted = {fg = "#ff7675"}
 
