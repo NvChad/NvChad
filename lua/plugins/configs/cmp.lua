73fb4b09f78c0da37fd724842807fb6c6b21a04d
Titor8115 <henryl8115@gmail.com>
2022-06-22 23:25:50 -0400

    Minor syntax fix

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -59,10 +59,10 @@ local options = {
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.close(),
-      ["<CR>"] = cmp.mapping.confirm {
+      ["<CR>"] = cmp.mapping.confirm({
          behavior = cmp.ConfirmBehavior.Insert,
          select = true,
-      },
+      }),
       ["<Tab>"] = cmp.mapping(function(fallback)
          if cmp.visible() then
             cmp.select_next_item()
