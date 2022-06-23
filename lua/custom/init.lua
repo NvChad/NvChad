54c5510b2b42eb6aae1e21a6e5cdbcc1581d4b3e
mehdi <mehdi@pop-os.localdomain>
2022-06-23 14:42:22 +0430

    feat: CommandBuffer and CommandResigter

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,9 +1,47 @@
 -- example file i.e lua/custom/init.lua
 -- load your globals, autocmds here or anything .__.
-
 -- lua, default settings
 
+vim.cmd([[
+    function! CommandBuffer(cmd)
+      redir => message
+      silent execute a:cmd
+      redir END
+      if empty(message)
+        echoerr "no output"
+      else
+        " use "new" instead of "tabnew" below if you prefer split windows instead of tabs
+        new
+        setlocal buftype=nofile bufhidden=wipe noswapfile nobuflisted nomodified
+        :nnoremap <buffer> q ZQ 
+        :nnoremap <buffer> <Esc> ZQ 
+        let message = trim(message)
+        silent put=message
+        silent execute "normal" "gg"
+        silent execute "normal" 3..'"_dd'
+      endif
+    endfunction
+    command! -nargs=+ -complete=command CommandBuffer call CommandBuffer(<q-args>)
+]])
 
--- vim.opt.list = true
-
+vim.cmd([[ 
+    function! CommandRegister(cmd)
+      redir => message 
+      silent execute a:cmd
+      redir END
+      let @" = trim(message)
+      if empty(message)
+        echoerr "no output."
+      else
+        echomsg "copied."
+      endif
+    endfunction
+    command! -nargs=+ -complete=command CommandRegister call CommandRegister(<q-args>)
+]])
+-- if you set expr=true it runs the command.
+-- TODO: check whichkey
+vim.api.nvim_set_keymap('n', '<space>cb', ':CommandBuffer ', {expr = false, noremap = true}) -- command to buffer
+vim.api.nvim_set_keymap('n', '<space>cr', ':CommandRegister ', {expr = false, noremap = true}) -- command to register
+vim.api.nvim_set_keymap('n', '<space>ub', ':CommandBuffer !', {expr = false, noremap = true}) -- unix to buffer
+vim.api.nvim_set_keymap('n', '<space>ur', ':CommandRegister !', {expr = false, noremap = true}) -- unix to register
 
