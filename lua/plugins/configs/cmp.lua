5e4b2e6a117bd62a29f02b4825d1f1b1f2f21172
siduck <siduck@tutanota.com>
2022-06-24 10:24:45 +0530

    avoid cmp to take over "<CR>" #1258

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -60,8 +60,8 @@ local options = {
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.close(),
       ["<CR>"] = cmp.mapping.confirm {
-         behavior = cmp.ConfirmBehavior.Insert,
-         select = true,
+         behavior = cmp.ConfirmBehavior.Replace,
+         select = false,
       },
       ["<Tab>"] = cmp.mapping(function(fallback)
          if cmp.visible() then
