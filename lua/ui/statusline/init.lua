c19398b3f6254626b866e7e7217357d7cbf8499c
siduck <siduck@tutanota.com>
2022-07-04 11:51:43 +0530

    fix  #1309

--- lua/ui/statusline/init.lua
+++ lua/ui/statusline/init.lua
@@ -1,19 +1,22 @@
 local modules = require "ui.statusline.modules"
-
 local user_override = require("core.utils").load_config().ui.statusline.override
 modules = vim.tbl_deep_extend("force", modules, user_override)
 
-return table.concat {
-   modules.mode(),
-   modules.fileInfo(),
-   modules.git(),
+return {
+   run = function()
+      return table.concat {
+         modules.mode(),
+         modules.fileInfo(),
+         modules.git(),
 
-   "%=",
-   modules.LSP_progress(),
-   "%=",
+         "%=",
+         modules.LSP_progress(),
+         "%=",
 
-   modules.LSP_Diagnostics(),
-   modules.LSP_status() or "",
-   modules.cwd(),
-   modules.cursor_position(),
+         modules.LSP_Diagnostics(),
+         modules.LSP_status() or "",
+         modules.cwd(),
+         modules.cursor_position(),
+      }
+   end,
 }
