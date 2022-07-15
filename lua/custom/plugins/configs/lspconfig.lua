b167677bcac365f60ab0b4605fb96e0df8f1c011
Mehdi Moradi <mahdi.moradi.w@gmail.com>
2022-07-15 23:02:07 +0430

    change broken lsp ui stuff

--- lua/custom/plugins/configs/lspconfig.lua
+++ lua/custom/plugins/configs/lspconfig.lua
@@ -1,5 +1,36 @@
 local M = {}
 
+-- overriding nvchad_ui/lsp.lua
+-- TODO: there is not better place to do it?
+vim.diagnostic.config {
+   virtual_text = false,
+   float = {
+       source = "always",
+       border = "rounded",
+   },
+   signs = false,
+   underline = true,
+   update_in_insert = false,
+}
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+   border = "rounded",
+})
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+   border = "rounded",
+   focusable = false,
+   relative = "cursor",
+})
+
+local win = require "lspconfig.ui.windows"
+local _default_opts = win.default_opts
+
+win.default_opts = function(options)
+   local opts = _default_opts(options)
+   opts.border = "rounded"
+   return opts
+end
+-- end of UI customization
+
 M.setup_lsp = function(on_attach, capabilities)
   local lspconfig = require "lspconfig"
   local lsp_settings = require "custom.lsp"
