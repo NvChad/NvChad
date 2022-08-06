b3d3c5a7dd4a5abccc6d17b199b4e6ad66fcedfc
nullchilly <nullchilly@gmail.com>
2022-08-06 10:58:05 +0700

    Don't load packer at startup

--- init.lua
+++ init.lua
@@ -4,6 +4,18 @@ require "core.options"
 require("core.utils").load_mappings()
 
 -- setup packer + plugins
-require("core.packer").bootstrap()
+local fn = vim.fn
+local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+  print "Cloning packer .."
+  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+
+  -- install plugins + compile their configs
+  vim.cmd "packadd packer.nvim"
+  require "plugins"
+  vim.cmd "PackerSync"
+end
 
 pcall(require, "custom")
