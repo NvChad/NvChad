322e408f3d7e50309f8da5a8d62f18c0b0fa0fca
Sidhanth Rathod <siduck@tutanota.com>
2022-08-07 15:44:40 +0530

    Update packer.lua

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -19,6 +19,23 @@ M.options = {
 -- merge overrides if there are any
 M.options = require("core.utils").load_override(M.options, "wbthomason/packer.nvim")
 
+-- boostrap packer + install & load all the plugins 
+M.bootstrap = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
+
+  if fn.empty(fn.glob(install_path)) > 0 then
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+    print "Cloning packer .."
+    fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+
+    -- install plugins + compile their configs
+    vim.cmd "packadd packer.nvim"
+    require "plugins"
+    vim.cmd "PackerSync"
+  end
+end
+
 M.run = function(plugins)
   local present, packer = pcall(require, "packer")
 
