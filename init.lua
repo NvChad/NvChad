2910ad8bd20d100689d9601d23f1bb27bc3493f2
Sidhanth Rathod <siduck@tutanota.com>
2022-08-07 15:45:27 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,20 +2,6 @@ require "core"
 require "core.options"
 
 require("core.utils").load_mappings()
-
--- setup packer + plugins
-local fn = vim.fn
-local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
-  print "Cloning packer .."
-  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
-
-  -- install plugins + compile their configs
-  vim.cmd "packadd packer.nvim"
-  require "plugins"
-  vim.cmd "PackerSync"
-end
+require("core.packer").bootstrap() -- bootstrap packer & load plugins
 
 pcall(require, "custom")
