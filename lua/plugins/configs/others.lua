3b39cc1cf8883ecb91df5ffb2bdc2fdacbebfa7d
siduck <siduck@tutanota.com>
2022-09-02 11:05:50 +0530

    BREAKING CHANGE : Simplify plugin management even more (#1518)

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -2,14 +2,6 @@ local M = {}
 
 local load_override = require("core.utils").load_override
 
-M.nvchad_ui = function()
-  local present, nvchad_ui = pcall(require, "nvchad_ui")
-
-  if present then
-    nvchad_ui.setup()
-  end
-end
-
 M.autopairs = function()
   local present1, autopairs = pcall(require, "nvim-autopairs")
   local present2, cmp = pcall(require, "cmp")
@@ -168,6 +160,24 @@ M.devicons = function()
 
     devicons.setup(options)
   end
+end
+
+M.packer_init = function()
+  return {
+    auto_clean = true,
+    compile_on_sync = true,
+    git = { clone_timeout = 6000 },
+    display = {
+      working_sym = "ﲊ",
+      error_sym = "✗ ",
+      done_sym = " ",
+      removed_sym = " ",
+      moved_sym = "",
+      open_fn = function()
+        return require("packer.util").float { border = "single" }
+      end,
+    },
+  }
 end
 
 return M
