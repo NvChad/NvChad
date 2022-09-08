8defbccc3f4b215e710b446097299fe4bcd80f98
Lucario387 <hoangtun0810@gmail.com>
2022-09-08 20:49:47 +0900

    remove gitsigns autocmd on gitsigns load

--- lua/core/lazy_load.lua
+++ lua/core/lazy_load.lua
@@ -78,9 +78,11 @@ M.mason_cmds = {
 
 M.gitsigns = function()
   autocmd({ "BufRead" }, {
+    group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
     callback = function()
       vim.fn.system("git rev-parse " .. vim.fn.expand "%:p:h")
       if vim.v.shell_error == 0 then
+        vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
         vim.schedule(function()
           require("packer").loader "gitsigns.nvim"
         end)
