8a18f89b03b0aaf013c8832024687f6e981e7576
siduck <siduck@tutanota.com>
2022-09-11 01:07:44 +0530

    install binaries from mason.nvm & tsparsers automatically after nvchad install

--- init.lua
+++ init.lua
@@ -18,6 +18,15 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd "packadd packer.nvim"
   require "plugins"
   vim.cmd "PackerSync"
+
+  -- install binaries from mason.nvim & tsparsers
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "PackerComplete",
+    callback = function()
+      vim.cmd "bw | silent! MasonInstallAll" -- close packer window
+      require("packer").loader "nvim-treesitter"
+    end,
+  })
 end
 
 pcall(require, "custom")
