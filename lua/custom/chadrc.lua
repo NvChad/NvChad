37b60f3ecfb7af11c81d0e39ce14c435b6c783e8
ayushmishra <mail81mitthu@gmail.com>
2022-10-01 08:17:52 +0530

    moved all the plugins config to a diff file :))

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -7,29 +7,6 @@ M.ui = {
   transparency = true,
 }
 
-M.plugins = {
-
-  user = {
-   ["neovim/nvim-lspconfig"] = {
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.plugins.lspconfig"
-    end,
-    },
-    ["simrat39/rust-tools.nvim"] = {
-    after = "nvim-lspconfig",
-    config = function()
-      require "custom.plugins.rust-tools"
-    end,
-  },
-    ["sbdchd/neoformat"] = {},
-    ["kyazdani42/nvim-tree.lua"] = {
-      cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-      config = function()
-        require "custom.plugins.nvim-tree"
-      end,
-    },
-  }
-}
+M.plugins = require "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,21 @@
+return {
+   ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+    },
+    ["simrat39/rust-tools.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.rust-tools"
+    end,
+  },
+    ["sbdchd/neoformat"] = {},
+    ["kyazdani42/nvim-tree.lua"] = {
+      cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+      config = function()
+        require "custom.plugins.nvim-tree"
+      end,
+    },
+}
