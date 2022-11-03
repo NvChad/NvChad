c6252937b207e40e72a6d646900c86ab4cd664a1
Uy Ha <hchanuy@gmail.com>
2022-11-03 09:02:33 +0000

    use rev-parse instead of status

--- lua/core/lazy_load.lua
+++ lua/core/lazy_load.lua
@@ -65,7 +65,7 @@ M.gitsigns = function()
   autocmd({ "BufRead" }, {
     group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
     callback = function()
-      vim.fn.system("git -C " .. vim.fn.expand "%:p:h" .. " status")
+      vim.fn.system("git -C " .. vim.fn.expand "%:p:h" .. " rev-parse")
       if vim.v.shell_error == 0 then
         vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
         vim.schedule(function()
