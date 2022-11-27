c55dea95933081e0ecc234e898920e32b42c204c
siduck <siduck@tutanota.com>
2022-11-27 19:00:36 +0530

    add helper comments for statusline options

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -32,8 +32,10 @@ M.ui = {
 
   ------------------------------- nvchad_ui modules -----------------------------
   statusline = {
-    theme = "default",
-    separator_style = "default", -- default/round/block/arrow
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
     overriden_modules = nil,
   },
 
