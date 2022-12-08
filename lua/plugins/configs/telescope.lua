7b43324f4f6d5a6b7f49abdb9aeebccd1e299f4f
bingjie6 <bingjie6@staff.weibo.com>
2022-12-08 10:18:21 +0800

    为telescope添加快捷键

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -55,7 +55,15 @@ local options = {
     -- Developer configurations: Not meant for general override
     buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
     mappings = {
-      n = { ["q"] = require("telescope.actions").close },
+      n = {
+        ["q"] = require("telescope.actions").close,
+        ["d"] = require("telescope.actions").delete_buffer,
+      },
+      i = {
+        ["<c-u>"] = false,
+        ["<c-a>"] = { "<home>", type = "command" },
+        ["<c-e>"] = { "<end>", type = "command" },
+      },
     },
   },
 
