926d160966008a47667411a9fc889225453803a3
bingjie6 <bingjie6@staff.weibo.com>
2022-12-14 16:47:30 +0800

    加速telescope

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -77,12 +77,21 @@ local options = {
   },
 
   extensions_list = { "themes", "terms" },
-   extensions = {
+  extensions = {
     xray23 = {
-     -- location to store session files, default is vim.fn.stdpath("data") .. "/vimSession"
-     sessionDir = vim.fn.stdpath("data") .. "/sessions",
+      -- location to store session files, default is vim.fn.stdpath("data") .. "/vimSession"
+      sessionDir = vim.fn.stdpath "data" .. "/sessions",
     },
-   },
+    extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        -- the default case_mode is "smart_case"
+      },
+    },
+  },
 }
 
 -- check for any override
