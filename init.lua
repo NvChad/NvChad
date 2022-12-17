8babf12991a2baaf6178f56467dfc6b141f4b846
siduck <siduck@tutanota.com>
2022-12-17 17:04:52 +0530

    avoid swallowing custom/init.lua errors by pcall

--- init.lua
+++ init.lua
@@ -41,6 +41,10 @@ if fn.empty(fn.glob(install_path)) > 0 then
   })
 end
 
-pcall(require, "custom")
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
+end
 
 require("core.utils").load_mappings()
