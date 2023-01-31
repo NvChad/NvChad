d094838842907bb72200689c8967ab6aa763242e
bingjie6 <bingjie6@staff.weibo.com>
2023-01-31 20:17:49 +0800

    所有buffer的内容都可以补全

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -91,7 +91,14 @@ local options = {
   sources = {
     { name = "luasnip" },
     { name = "nvim_lsp" },
-    { name = "buffer" },
+    {
+      name = "buffer",
+      option = {
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end,
+      },
+    },
     { name = "nvim_lua" },
     { name = "path" },
   },
