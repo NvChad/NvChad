3b8967ade53dbf4ec7aa5dc0c8ec598a77ffd184
bingjie6 <bingjie6@staff.weibo.com>
2023-02-03 14:13:24 +0800

    nvim-cmp 支持supertab mapping

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -30,6 +30,12 @@ cmp_window.info = function(self)
   return info
 end
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local options = {
   window = {
     completion = {
@@ -55,9 +61,11 @@ local options = {
   mapping = {
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<Up>"] = cmp.mapping.select_prev_item(),
+    ["<Down>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<M-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
@@ -68,6 +76,8 @@ local options = {
         cmp.select_next_item()
       elseif require("luasnip").expand_or_jumpable() then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
