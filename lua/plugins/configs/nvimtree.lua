2a7a8e5a219aebb566d5785e57dae87b68e6f4fe
neo-f <tmpgfw@gmail.com>
2023-02-09 13:14:06 +0800

    update nvim-tree option names

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -9,10 +9,10 @@ local options = {
   hijack_netrw = true,
   hijack_cursor = true,
   hijack_unnamed_buffer_when_opening = false,
-  update_cwd = true,
+  sync_root_with_cwd = true,
   update_focused_file = {
     enable = true,
-    update_cwd = false,
+    update_root = false,
   },
   view = {
     adaptive_size = false,
