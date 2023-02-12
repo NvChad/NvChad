e72479081d82fce017468998b78c84343c0eb0df
ariefra <arief.r.id+github@gmail.com>
2023-02-12 17:46:20 +0700

    Fix sumneko_lua deprecation, change to lua_ls

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -37,7 +37,7 @@ M.capabilities.textDocument.completion.completionItem = {
   },
 }
 
-require("lspconfig").sumneko_lua.setup {
+require("lspconfig").lua_ls.setup {
   on_attach = M.on_attach,
   capabilities = M.capabilities,
 
