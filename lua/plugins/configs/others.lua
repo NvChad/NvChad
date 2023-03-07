d1c47a441c930c1af4c1c99cf3aff70ecf356a27
Loc Le Phat <lpl212757@gmail.com>
2023-03-08 03:31:07 +0700

    'tmp'

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -48,7 +48,7 @@ M.blankline = function()
     buftype_exclude = { "terminal" },
     show_trailing_blankline_indent = false,
     show_first_indent_level = false,
-    show_current_context = true,
+    show_current_context = false,
     show_current_context_start = true,
   }
 
