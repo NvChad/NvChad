a810dc0f72e196e5a318d196e321f9e1dfca361c
Loc Le Phat <lpl212757@gmail.com>
2023-03-10 17:52:02 +0700

    [MOD] Mark .md as project also

--- lua/plugins/configs/projects.lua
+++ lua/plugins/configs/projects.lua
@@ -17,7 +17,7 @@ local options = {
 
   -- All the patterns used to detect root dir, when **"pattern"** is in
   -- detection_methods
-  patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+  patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".md" },
 
   -- Table of lsp clients to ignore by name
   -- eg: { "efm", ... }
