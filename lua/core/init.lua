84bdc2335cf46032f7b8fa9a8b2a6aa9901751e6
siduck <siduck@tutanota.com>
2023-03-23 16:09:43 +0530

    save nvchad cache at nvim data dir
    many a time the user clears the cache dir and that results in deletion of base46 cache too which would prevent nvchad to function

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -4,7 +4,7 @@ local config = require("core.utils").load_config()
 
 -------------------------------------- globals -----------------------------------------
 g.nvchad_theme = config.ui.theme
-g.base46_cache = vim.fn.stdpath "cache" .. "/nvchad/base46/"
+g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 g.toggle_theme_icon = "   "
 g.transparency = config.ui.transparency
 
