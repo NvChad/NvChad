1bbfecf8810aaad3b6fedcbed675c8851fa3e93c
Ashish <ashish.2017@hotmail.com>
2023-04-02 21:26:21 +0100

    gitsign: handling file path with spaces

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -101,7 +101,7 @@ local default_plugins = {
       vim.api.nvim_create_autocmd({ "BufRead" }, {
         group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
         callback = function()
-          vim.fn.system("git -C " .. vim.fn.expand "%:p:h" .. " rev-parse")
+          vim.fn.system("git -C " .. '"' .. vim.fn.expand "%:p:h" .. '"' .. " rev-parse")
           if vim.v.shell_error == 0 then
             vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
             vim.schedule(function()
