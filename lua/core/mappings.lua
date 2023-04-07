b5cf9fde0e64d963a776c80f87609a3425771e04
Shubham K <53286173+sk02241994@users.noreply.github.com>
2023-04-07 22:59:13 +0530

    Added current buffer fuzzy find key mapping (#1903)
    * Added current buffer fuzzy find key mapping
    
    * change telescope fuzzy mapping to <leader>fz
    
     Co-authored-by: Sidhanth Rathod <siduck@tutanota.com>

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -265,6 +265,7 @@ M.telescope = {
     ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "find buffers" },
     ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "help page" },
     ["<leader>fo"] = { "<cmd> Telescope oldfiles <CR>", "find oldfiles" },
+    ["<leader>fz"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "find in current buffer" },
 
     -- git
     ["<leader>cm"] = { "<cmd> Telescope git_commits <CR>", "git commits" },
