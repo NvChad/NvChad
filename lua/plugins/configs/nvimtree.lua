12f26e91efadd0e9f6140af5f4e0d27f9e10506f
siduck <siduck@tutanota.com>
2023-04-12 14:12:32 +0530

    rm depcreated option from nvimtree config

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -16,7 +16,6 @@ local options = {
     adaptive_size = false,
     side = "left",
     width = 30,
-    hide_root_folder = true,
     preserve_window_proportions = true,
   },
   git = {
@@ -32,6 +31,7 @@ local options = {
     },
   },
   renderer = {
+    root_folder_label = false,
     highlight_git = false,
     highlight_opened_files = "none",
 
