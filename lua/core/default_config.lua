1cb237fa78e63b80132cbb6a05f45eb074d3d934
siduck <siduck@tutanota.com>
2023-05-18 07:58:57 +0530

    rm outdated icons

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -64,8 +64,8 @@ M.ui = {
 
     buttons = {
       { "  Find File", "Spc f f", "Telescope find_files" },
-      { "  Recent Files", "Spc f o", "Telescope oldfiles" },
-      { "  Find Word", "Spc f w", "Telescope live_grep" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
       { "  Bookmarks", "Spc b m", "Telescope marks" },
       { "  Themes", "Spc t h", "Telescope themes" },
       { "  Mappings", "Spc c h", "NvCheatsheet" },
