fe64c9a98cb3a6f7dc2fed6e5e34ff89d14ed347
Josue Romero <josueromram3@gmail.com>
2023-05-25 00:00:34 -0500

    Last update

--- lua/core/mappings_windows.lua
+++ lua/core/mappings_windows.lua
@@ -15,16 +15,16 @@ M.general = {
     ["<C-k>"] = { "<Up>", "Move up" },
 
     -- compile program cpp
-    ["<F1>"] = {"<Esc> :w<CR> :!g++ % -o ~/workspace/build/sol.out -std=c++20 -Wall -Wextra -Wshadow -Wpedantic -Weffc++ -Djosuerom -Wno-unused-result -Wno-char-subscripts -Wfloat-equal -Wconversion -Wformat=2 -Wvla -Wduplicated-cond -Wlogical-op -Wredundant-decls -ggdb3 -D_GLIBCXX_DEBUG<CR>", "Compile cpp file"},
+    ["<F1>"] = {"<Esc> :w<CR> :!g++ % -o C:\\Users\\jr3\\workspace\\build\\sol.exe -std=c++20 -march=native -Wall -Wextra -Wl,--stack=256000000 -Wpedantic -Wshadow -Weffc++ -Wfloat-equal -Wconversion -Djosuerom -Wformat=2 -Wvla -Wduplicated-cond -Wredundant-decls -march=native -Wall -Wextra -Wl,--stack=256000000 -Wpedantic -Wshadow -Weffc++ -Wfloat-equal -Wconversion -Djosuerom -Wformat=2 -Wvla -Wduplicated-cond -Wredundant-decls -ggdb3 -D_GLIBCXX_DEBUG<CR>", "Compile cpp file"},
 
     -- compile and run program cpp
-    ["<F2>"] = {"<Esc> :w<CR> :!g++ % -o ~/workspace/build/sol.out -std=c++20 -Wall -Wextra -Wshadow -Wpedantic -Weffc++ -Djosuerom -Wno-unused-result -Wno-char-subscripts -Wfloat-equal -Wconversion -Wformat=2 -Wvla -Wduplicated-cond -Wlogical-op -Wredundant-decls -ggdb3 -D_GLIBCXX_DEBUG<CR> :!~/workspace/build/sol.out < ~/workspace/sample/input<CR>", "Compile and run cpp file with input"},
+    ["<F2>"] = { "<Esc> :w<CR> :!g++ % -o C:\\Users\\jr3\\workspace\\build\\sol.exe -std=c++20 -march=native -Wall -Wextra -Wl,--stack=256000000 -Wpedantic -Wshadow -Weffc++ -Wfloat-equal -Wconversion -Djosuerom -Wformat=2 -Wvla -Wduplicated-cond -Wredundant-decls -march=native -Wall -Wextra -Wl,--stack=256000000 -Wpedantic -Wshadow -Weffc++ -Wfloat-equal -Wconversion -Djosuerom -Wformat=2 -Wvla -Wduplicated-cond -Wredundant-decls -ggdb3 -D_GLIBCXX_DEBUG<CR> :!C:\\Users\\jr3\\workspace\\build\\sol.exe < C:\\Users\\jr3\\workspace\\sample\\input<CR>", "Compile and run cpp" },
 
     -- compile and run program java
-    ["<F3>"] = {"<Esc> :w<CR> :!java % < ~/workspace/sample/input<CR>", "Compile and run java file with input"},
+    ["<F3>"] = { "<Esc> :w<CR> :!java % < C:\\Users\\jr3\\workspace\\sample\\input<CR>", "Compile and run java file with input" },
 
     -- open standard input file
-    ["<F4>"] = { "<Esc> :w<CR> :e ~/workspace/sample/input<CR>", "Open standard input file" },
+    ["<F4>"] = { "<Esc> :w<CR> :e C:\\Users\\jr3\\workspace\\sample\\input<CR>", "Open standard input file" },
   },
 
   n = {
@@ -36,16 +36,16 @@ M.general = {
     ["<C-k>"] = { "<C-w>k", "Window up" },
 
     -- compile program cpp
-    ["<F1>"] = {":w<CR> :!g++ % -o ~/workspace/build/sol.out -std=c++20 -Wall -Wextra -Wshadow -Wpedantic -Weffc++ -Djosuerom -Wno-unused-result -Wno-char-subscripts -Wfloat-equal -Wconversion -Wformat=2 -Wvla -Wduplicated-cond -Wlogical-op -Wredundant-decls -ggdb3 -D_GLIBCXX_DEBUG<CR>", "Compile cpp file"},
+    ["<F1>"] = {":w<CR> :!g++ % -o C:\\Users\\jr3\\workspace\\build\\sol.exe -std=c++20 -march=native -Wall -Wextra -Wl,--stack=256000000 -Wpedantic -Wshadow -Weffc++ -Wfloat-equal -Wconversion -Djosuerom -Wformat=2 -Wvla -Wduplicated-cond -Wredundant-decls -march=native -Wall -Wextra -Wl,--stack=256000000 -Wpedantic -Wshadow -Weffc++ -Wfloat-equal -Wconversion -Djosuerom -Wformat=2 -Wvla -Wduplicated-cond -Wredundant-decls -ggdb3 -D_GLIBCXX_DEBUG<CR>", "Compile cpp file"},
 
     -- run program cpp
-    ["<F2>"] = {":!~/workspace/build/sol.out < ~/workspace/sample/input<CR>", "Run sol.out file with input"},
+    ["<F2>"] = {":!C:\\Users\\jr3\\workspace\\build\\sol.exe < C:\\Users\\jr3\\workspace\\sample\\input<CR>", "Run sol.exe file with input"},
 
     -- run program java
-    ["<F3>"] = {":w<CR> :!java % < ~/workspace/sample/input<CR>", "Compile and run java file with input"},
+    ["<F3>"] = {":w<CR> :!java % < C:\\Users\\jr3\\workspace\\sample\\input<CR>", "Compile and run java file with input"},
 
     -- open standard input file
-    ["<F4>"] = { ":w<CR> :e ~/workspace/sample/input<CR>", "Open standard input file" },
+    ["<F4>"] = { ":w<CR> :e C:\\Users\\jr3\\workspace\\sample\\input<CR>", "Open standard input file" },
 
     -- save and exit
     ["<C-s>"] = { "<cmd> w <CR>", "Save file" },
