f8a489ea1a670569eead0fe5651c0c48add743e8
siduck <siduck@tutanota.com>
2023-06-01 22:07:27 +0530

    include editor config in lazy disabled_plugins list

--- lua/plugins/configs/lazy_nvim.lua
+++ lua/plugins/configs/lazy_nvim.lua
@@ -41,6 +41,7 @@ return {
         "compiler",
         "bugreport",
         "ftplugin",
+        "editorconfig",
       },
     },
   },
--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -51,12 +51,12 @@ end
 
 M.gitsigns = {
   signs = {
-    add          = { text = '│' },
-    change       = { text = '│' },
-    delete       = { text = '󰍵' },
-    topdelete    = { text = '‾' },
-    changedelete = { text = '~' },
-    untracked    = { text = '│' },
+    add = { text = "│" },
+    change = { text = "│" },
+    delete = { text = "󰍵" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
+    untracked = { text = "│" },
   },
   on_attach = function(bufnr)
     utils.load_mappings("gitsigns", { buffer = bufnr })
