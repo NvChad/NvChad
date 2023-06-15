ef164182a61b7c3b0f1d7fa636976e4c32a4c995
siduck <siduck@tutanota.com>
2023-06-15 08:01:57 +0530

    clean up!

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -4,17 +4,15 @@ local merge_tb = vim.tbl_deep_extend
 M.load_config = function()
   local config = require "core.default_config"
   local chadrc_path = vim.api.nvim_get_runtime_file("lua/custom/chadrc.lua", false)[1]
-  local default_mappings = require "core.mappings"
-  config.mappings = default_mappings
 
   if chadrc_path then
     local chadrc = dofile(chadrc_path)
 
-    config.mappings = M.remove_disabled_keys(chadrc.mappings, default_mappings)
+    config.mappings = M.remove_disabled_keys(chadrc.mappings, config.mappings)
     config = merge_tb("force", config, chadrc)
+    config.mappings.disabled = nil
   end
 
-  config.mappings.disabled = nil
   return config
 end
 
