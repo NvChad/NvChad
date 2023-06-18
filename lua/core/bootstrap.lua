461e7f3c4ed82c0f043590b33c18375d6ab387c3
siduck <siduck@tutanota.com>
2023-06-18 18:21:12 +0530

    Update bootstrap.lua

--- lua/core/bootstrap.lua
+++ lua/core/bootstrap.lua
@@ -34,6 +34,7 @@ M.gen_chadrc_template = function()
   if not vim.api.nvim_get_runtime_file("lua/custom/chadrc.lua", false)[1] then
     local path = vim.fn.stdpath "config" .. "/lua/custom/"
     local input = "N"
+
     if next(vim.api.nvim_list_uis()) then
       input = vim.fn.input "Do you want to install example custom config? (y/N) : "
     end
