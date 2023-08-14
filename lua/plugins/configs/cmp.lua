38cad47727a54f30e0a3a9478e56f78c0917e609
Loc Le Phat <lpl212757@gmail.com>
2023-08-15 03:35:42 +0700

    [MOD] Add cmp config for copilot

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -142,4 +142,21 @@ cmp.setup.cmdline(':', {
   })
 })
 
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
+cmp.setup({
+  mapping = {
+    ["<Tab>"] = vim.schedule_wrap(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      else
+        fallback()
+      end
+    end),
+  },
+})
+
 cmp.setup(options)
