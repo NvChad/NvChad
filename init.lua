92d8c3d22f8e315c2ed706ab19439fbf27b5e348
Jab <74190465+thisisjab@users.noreply.github.com>
2023-08-19 11:58:20 +0330

    fix: update init.lua to fix issues with incompatible bytecode

--- init.lua
+++ init.lua
@@ -16,6 +16,6 @@ if not vim.loop.fs_stat(lazypath) then
   require("core.bootstrap").lazy(lazypath)
 end
 
-dofile(vim.g.base46_cache .. "defaults")
+loadfile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
