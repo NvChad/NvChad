3f1e6d71d4c6c98380d5383d5e1bf1f6eaa3399f
siduck <siduck@tutanota.com>
2023-08-27 11:28:56 +0530

    make whichkey lazyloadable by its cmd | fix #2342

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -256,6 +256,7 @@ local default_plugins = {
     init = function()
       require("core.utils").load_mappings "whichkey"
     end,
+    cmd = "WhichKey",
     config = function(_, opts)
       dofile(vim.g.base46_cache .. "whichkey")
       require("which-key").setup(opts)
