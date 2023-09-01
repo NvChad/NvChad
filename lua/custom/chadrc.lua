2da20856206231e8ccdcb98ef3bc320ab5ebf440
Zerio <neoboij@gmail.com>
2023-09-02 01:09:11 +0200

    Better theme/text colors for transparency

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "tokyodark",
-  theme_toggle = { "tokyodark", "one_light" },
+  theme = "nightowl",
+  theme_toggle = { "nightowl", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
