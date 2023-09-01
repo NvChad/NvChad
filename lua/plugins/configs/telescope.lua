4dca950a07b6d36e25781069ad200139073050a6
Zerio <neoboij@gmail.com>
2023-09-01 21:14:18 +0200

    Ignore .git files in telescope

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -31,7 +31,7 @@ local options = {
       preview_cutoff = 120,
     },
     file_sorter = require("telescope.sorters").get_fuzzy_file,
-    file_ignore_patterns = { "node_modules" },
+    file_ignore_patterns = { "node_modules", ".git" },
     generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
     path_display = { "truncate" },
     winblend = 0,
