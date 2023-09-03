c2ea4af88fd4f3ba2e5924d6c2ba4a270f7a5f46
Zerio <neoboij@gmail.com>
2023-09-03 16:02:09 +0200

    presence: enable showing of line numbers

--- lua/custom/configs/presence.lua
+++ lua/custom/configs/presence.lua
@@ -6,7 +6,7 @@ require("presence").setup({
     client_id           = "793271441293967371",         -- Use your own Discord application client id (not recommended)
     log_level           = nil,                          -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
     debounce_timeout    = 10,                           -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-    enable_line_number  = false,                        -- Displays the current line number instead of the current project
+    enable_line_number  = true,                        -- Displays the current line number instead of the current project
     blacklist           = {},                           -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
     buttons             = true,                         -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
     file_assets         = {},                           -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
