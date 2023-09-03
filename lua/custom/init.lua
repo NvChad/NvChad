e79102d087d05f78d0fcaa2b7e7a3bba27dfa3a3
Zerio <neoboij@gmail.com>
2023-09-03 16:18:39 +0200

    Add command for opening current dir in explorer

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -5,3 +5,11 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+-- Misc
+function OpenExplorer()
+  vim.cmd "!explorer.exe . &"
+end
+
+vim.api.nvim_create_user_command("Exp", OpenExplorer, {})
+vim.api.nvim_create_user_command("Explorer", OpenExplorer, {})
