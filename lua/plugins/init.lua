617dca4dc11231138eb880b7c79253c17c9720f9
Zohir Benghalem <116302048+kayuxx@users.noreply.github.com>
2023-09-09 02:13:28 +0100

    add fzf telescope extension to improve sorting performance (#2371)
    * add fzf telescope extension to improve sorting performance
    
    * add fzf config tbl to the default tlsc config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -229,7 +229,7 @@ local default_plugins = {
 
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
     cmd = "Telescope",
     init = function()
       require("core.utils").load_mappings "telescope"
