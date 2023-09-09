b623db7ab6a72e222e13165dd136405aa0261b36
Zerio <neoboij@gmail.com>
2023-09-09 19:09:01 +0200

    Remove minimap

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -22,17 +22,6 @@ local plugins = {
   },
 
   {
-    "wfxr/minimap.vim",
-    opts = {},
-    lazy = false,
-    config = function()
-      vim.g.minimap_width = 10
-      vim.g.minimap_auto_start = 1
-      vim.g.minimap_auto_start_win_enter = 1
-    end,
-  },
-
-  {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
