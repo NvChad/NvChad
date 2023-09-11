649feda7a164e05aacd2cc33c82dbf8533af9e0b
Zerio <neoboij@gmail.com>
2023-09-11 17:55:13 +0200

    Fix issues with building `NvChad-customdata` when updating

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -69,6 +69,9 @@ local plugins = {
     build = function()
       os.execute("cd " .. vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/fivem && npm install && npm run build")
     end,
+    update = function()
+      os.execute("cd " .. vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/fivem && npm install && npm run build")
+    end,
     lazy = false
   },
 
