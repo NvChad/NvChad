72ad27c328f5e18d1483420a397f5c081a190a8b
siduck <siduck@tutanota.com>
2023-09-11 14:47:45 +0530

    Revert "rm useless luasnip calls"
    This reverts commit de8597731641e410ac78fa6395c98a01c532d091.

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -25,8 +25,16 @@ M.blankline = {
 M.luasnip = function(opts)
   require("luasnip").config.set_config(opts)
 
+  -- vscode format
+  require("luasnip.loaders.from_vscode").lazy_load()
   require("luasnip.loaders.from_vscode").lazy_load { paths = vim.g.vscode_snippets_path or "" }
+
+  -- snipmate format
+  require("luasnip.loaders.from_snipmate").load()
   require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snippets_path or "" }
+
+  -- lua format
+  require("luasnip.loaders.from_lua").load()
   require("luasnip.loaders.from_lua").lazy_load { paths = vim.g.lua_snippets_path or "" }
 
   vim.api.nvim_create_autocmd("InsertLeave", {
