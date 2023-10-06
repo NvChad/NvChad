acdf91d7b9facf593bb63392300f621652a0b84a
Doug Anderson444 <douganderson444@gmail.com>
2023-10-06 13:21:17 -0300

    add copilot.lua

---
+++ lua/custom/configs/copilot.lua
@@ -0,0 +1,45 @@
+local copilot = require "copilot"
+
+copilot.setup {
+  panel = {
+    enabled = true,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>",
+    },
+    layout = {
+      position = "bottom", -- | top | left | right
+      ratio = 0.4,
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true, -- default: false
+    debounce = 75,
+    keymap = {
+      accept = "<C-l>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+    },
+  },
+  filetypes = {
+    yaml = false,
+    markdown = true, -- default: false,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+  copilot_node_command = vim.fn.expand "$HOME" .. "/Programs/node-v18.18.0-linux-x64/bin/node", -- Node.js version must be > 16.x
+  server_opts_overrides = {},
+}
