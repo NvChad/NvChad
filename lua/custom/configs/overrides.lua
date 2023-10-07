0c4aeac3bae4835dbc6daf9c2bd3af6cbedc0fef
Panos Sakkos <panos.sakkos@panther.com>
2023-10-07 11:21:20 +0300

    show dotfiles in tree view by default

---
+++ lua/custom/configs/overrides.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+-- inline git blame
+M.gitsigns = {
+  current_line_blame = true,
+}
+
+-- syntax highlight
+M.treesitter = {
+  ensure_installed = {
+    "go",
+    "gosum",
+    "gomod",
+  },
+}
+
+return M
