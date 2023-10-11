810c9c5dc45450b469768db85af3a72900c807d7
Doug Anderson444 <douganderson444@gmail.com>
2023-10-11 12:33:30 -0300

    add autocmd for autoreload on file changes

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -5,3 +5,19 @@ vim.diagnostic.config {
     source = "always",
   },
 }
+
+-- vim command for reload buffer if written from elsewhere:
+vim.cmd [[
+  augroup autoread
+    autocmd!
+    autocmd FocusGained,BufEnter,CursorHold,CursorHoldI * if mode() != 'c' | checktime | endif
+  augroup END
+]]
+
+-- notification after file change
+vim.cmd [[
+  augroup filechanged
+    autocmd!
+    autocmd FileChangedShellPost * echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None
+  augroup END
+]]
