b0bc80b30000e5b0b78b3d3e23a4fb04c9a9bcbd
Panos Sakkos <panos.sakkos@panther.com>
2023-10-11 11:25:18 +0300

    change statusline theme

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,12 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'everforest_light' }
+M.ui = {
+  theme = 'everforest_light',
+  statusline = {
+    theme = "vscode_colored",
+  },
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
