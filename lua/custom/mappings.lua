02d8716dd510d1204153fc8e1e1e73e487c94e67
Panos Sakkos <panos.sakkos@panther.com>
2023-10-13 00:51:55 +0300

    remove unused nvchad core mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,6 +1,43 @@
 ---@type MappingsTable
 local M = {}
 
+-- Disabled NvChad core mappings
+M.disabled = {
+  t = {
+    ["<C-x>"] = "",
+    ["<A-i>"] = "",
+    ["<A-h>"] = "",
+    ["<A-v>"] = "",
+  },
+
+  v = {
+    ["<Up>"] = "",
+    ["<Down>"] = "",
+  },
+
+  n = {
+    ["<Up>"] = "",
+    ["<Down>"] = "",
+    ["<leader>fh"] = "",
+    ["<leader>th"] = "",
+    ["<leader>ma"] = "",
+    ["<leader>fo"] = "",
+    ["<leadr>fz"] = "",
+    ["<A-i>"] = "",
+    ["<A-h>"] = "",
+    ["<A-v"] = "",
+    ["<leader>wK"] = "",
+    ["<leader>wk"] = "",
+    ["<leader>cc"] = "",
+    ["]c"] = "",
+    ["[c"] = "",
+    ["<leader>rh"] = "",
+    ["<leader>ph"] = "",
+    ["<leader>gb"] = "",
+    ["<leader>td"] = "",
+  },
+}
+
 M.general = {
   t = {
     ["<ESC>"] = { "<C-\\><C-n>", "escape terminal mode", opts = { nowait = true } },
