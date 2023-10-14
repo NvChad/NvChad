a2d0f497a78eadb59f29051e44641cf8c4c3ed1f
Zerio <neoboij@gmail.com>
2023-10-14 20:41:16 +0200

    Update plugins.lua

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -91,6 +91,47 @@ local plugins = {
   },
 
   {
+    "nvim-treesitter/nvim-treesitter-context",
+
+    config = function()
+      require("treesitter-context").setup({
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      })
+    end,
+
+    lazy = false,
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        autotag = {
+          enable = true,
+        },
+      })
+    end,
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+
+    lazy = false,
+  },
+
+  {
     "Z3rio/FiveM.nvim",
 
     config = function()
