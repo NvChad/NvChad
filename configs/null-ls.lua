eed157fa12869e3acefa1c2496458edbbe5c4fc6
Panos Sakkos <panos.sakkos@panther.com>
2023-10-27 12:08:50 +0300

    format file

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,4 +1,4 @@
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local opts = {
@@ -12,16 +12,16 @@ local opts = {
     null_ls.builtins.formatting.yamlfmt,
   },
   on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds {
         group = augroup,
         buffer = bufnr,
-      })
+      }
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
+          vim.lsp.buf.format { bufnr = bufnr }
         end,
       })
     end
