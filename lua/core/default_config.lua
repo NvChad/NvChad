8817a4b75537ed2380ca96346d127992e3fcf943
NSSPKrishna <pniladhuri@newrelic.com>
2023-11-13 19:00:40 +0530

    chore: Minor

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -12,7 +12,7 @@ M.ui = {
   changed_themes = {},
   theme_toggle = { "onedark", "one_light" },
   theme = "onedark", -- default theme
-  transparency = false,
+  transparency = true,
   lsp_semantic_tokens = false, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
 
   -- https://github.com/NvChad/base46/tree/v2.0/lua/base46/extended_integrations
--- lua/plugins/configs/lazy_nvim.lua
+++ lua/plugins/configs/lazy_nvim.lua
@@ -1,6 +1,6 @@
 return {
   defaults = { lazy = true },
-  install = { colorscheme = { "nvchad" } },
+  install = { colorscheme = { "nightfox" } },
 
   ui = {
     icons = {
