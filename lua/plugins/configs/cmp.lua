b8a42f5819356d250303ab5d9ffbe8634ebbc30e
wochap <gean.marroquin@gmail.com>
2023-11-12 23:02:34 -0500

    refactor

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -52,12 +52,12 @@ local options = {
   window = {
     completion = {
       side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
-      winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None,IncSearch:None,CurSearch:None",
+      winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
       scrollbar = false,
     },
     documentation = {
       border = border "CmpDocBorder",
-      winhighlight = "Normal:CmpDoc,Search:None,IncSearch:None,CurSearch:None",
+      winhighlight = "Normal:CmpDoc",
     },
   },
   snippet = {
