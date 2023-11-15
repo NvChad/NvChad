219d5be55911c96cf724164f82dafea0eed5080b
Panos Sakkos <panos.sakkos@gmail.com>
2023-11-15 15:16:02 +0200

    list tabs spaces and eol

--- init.lua
+++ init.lua
@@ -5,4 +5,4 @@ opt.swapfile = false
 opt.colorcolumn = "80"
 opt.relativenumber = true
 opt.list = true
-opt.listchars = "tab:> ,lead:·,space:·,trail:·,nbsp:+,eol:¬"
+opt.listchars = "tab:➝ ,lead:·,space:·,trail:·,nbsp:+,eol:¬"
