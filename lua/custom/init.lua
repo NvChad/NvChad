9fcafb8bb4c02848b468de10f404fa2f25493af8
Edwin Muraya (localSetup) <edwinmuraya@microsoft.com>
2023-11-19 00:39:24 +0300

    Add: Add lsp configuration, formating and linting

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,12 +1,8 @@
+vim.api.nvim_set_keymap("n", "<leader>co", ":lua search_dotfiles()<CR>", { noremap = true, silent = true })
 
-local function search_dotfiles()
-  require('telescope.builtin').find_files {
-    -- exclude Library and Binary from Files
-    find_command = { "git", "--git-dir=" .. os.getenv("HOME") .. "/.cfg/.git", "ls-files", "--" , ":!:Lib*", ":!:bin*" }
-  }
-end
-
-vim.api.nvim_set_keymap('n', '<leader>co', ':lua search_dotfiles()<CR>', {noremap = true, silent = true})
-
-
-vim.api.nvim_set_keymap('n', '<leader>mn', ':lua require("telescope.builtin").find_files({ cwd = "/Users/edwinmuraya/Library/CloudStorage/OneDrive-Personal/learning_notes" })<cr>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>mn",
+  ':lua require("telescope.builtin").find_files({ cwd = "/Users/edwinmuraya/Library/CloudStorage/OneDrive-Personal/learning_notes" })<cr>',
+  { noremap = true, silent = true }
+)
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,8 +1,8 @@
-local is_macbook = vim.fn.has("mac") == 1
+local is_macbook = vim.fn.has "mac" == 1
 local one_drive_notes
 
 if is_macbook then
-   one_drive_notes = "/Users/edwinmuraya/Library/CloudStorage/OneDrive-Personal/learning_notes"
+  one_drive_notes = "/Users/edwinmuraya/Library/CloudStorage/OneDrive-Personal/learning_notes"
 else
   one_drive_notes = ""
 end
@@ -13,7 +13,7 @@ local plugins = {
     event = "VeryLazy",
     requires = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("rest-nvim").setup({
+      require("rest-nvim").setup {
         -- Open request results in a horizontal split
         result_split_horizontal = false,
         -- Keep the http file buffer above|left when split horizontal|vertical
@@ -40,19 +40,19 @@ local plugins = {
           formatters = {
             json = "jq",
             html = function(body)
-              return vim.fn.system({"tidy", "-i", "-q", "-"}, body)
-            end
+              return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+            end,
           },
         },
         -- Jump to request line on run
         jump_to_request = false,
-        env_file = '.env',
+        env_file = ".env",
         custom_dynamic_variables = {},
         yank_dry_run = true,
-      })
-    end
+      }
+    end,
   },
-   {
+  {
     "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
     dependencies = { "nvim-lua/plenary.nvim" },
@@ -73,12 +73,17 @@ local plugins = {
     end,
   },
   {
+    "nvim-telescope/telescope.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
+  },
+  {
     "rcarriga/nvim-dap-ui",
     event = "VeryLazy",
     dependencies = "mfussenegger/nvim-dap",
     config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
+      local dap = require "dap"
+      local dapui = require "dapui"
       dapui.setup()
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
@@ -89,7 +94,7 @@ local plugins = {
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close()
       end
-    end
+    end,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
@@ -99,27 +104,19 @@ local plugins = {
       "mfussenegger/nvim-dap",
     },
     opts = {
-      handlers = {}
+      handlers = {},
     },
   },
   {
     "mfussenegger/nvim-dap",
     config = function(_, _)
-      require("core.utils").load_mappings("dap")
-    end
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return require "custom.configs.null-ls"
+      require("core.utils").load_mappings "dap"
     end,
   },
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#omnisharp
         omnisharp = {
           organize_imports_on_format = true,
           enable_import_completion = true,
@@ -133,62 +130,179 @@ local plugins = {
   },
   {
     "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      local mason = require "mason"
+      local mason_lspconfig = require "mason-lspconfig"
+      local mason_tool_installer = require "mason-tool-installer"
+
+      mason_tool_installer.setup {
+        ensure_installed = {
+          "prettier",
+          "stylua",
+          "isort",
+          "black",
+          "pylint",
+          "eslint_d",
+        },
+      }
+    end,
     opts = {
       ensure_installed = {
         "clangd",
         "clang-format",
         "codelldb",
         "netcoredbg",
-        "rust-analyzer"
-      }
-    }
-  },{
-     "rust-lang/rust.vim",
-      ft="rust",
-      init=function()
-        vim.g.rustfmt_autosave = 1
-      end
-  },{
+        "rust-analyzer",
+        "tailwindcss",
+      },
+    },
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
     "simrat39/rust-tools.nvim",
+
     ft = "rust",
     dependencies = "neovim/nvim-lspconfig",
-    opts = function ()
+    opts = function()
       return require "custom.configs.rust_tools"
     end,
     config = function(_, opts)
-      require('rust-tools').setup(opts)
-    end
-  }, {
-       "github/copilot.vim",
-        event = "VeryLazy",
+      require("rust-tools").setup(opts)
+    end,
   },
   {
-    'nvim-orgmode/orgmode',
+    "github/copilot.vim",
+    event = "VeryLazy",
+  },
+  {
+    "nvim-orgmode/orgmode",
     event = "VeryLazy",
     dependencies = "nvim-treesitter/nvim-treesitter",
-    ft = {'org'},
+    ft = { "org" },
     config = function()
-            require('orgmode').setup_ts_grammar();
-            require('orgmode').setup{
-             org_agenda_files = { one_drive_notes, '~/.system_notes/**/*'},
-             org_default_notes_file = one_drive_notes .. "/org_refile.org",
+      require("orgmode").setup_ts_grammar()
+      require("orgmode").setup {
+        org_agenda_files = { one_drive_notes, "~/.system_notes/**/*" },
+        org_default_notes_file = one_drive_notes .. "/org_refile.org",
       }
-    end
+    end,
   },
   {
-    'nvim-treesitter/nvim-treesitter',
-     event = "VeryLazy",
-     config = function ()
-      require'nvim-treesitter.configs'.setup {
-       ensure_installed = { "c", "cpp", "cmake" ,"bash", "c_sharp", "rust", "vim", "yaml", "org", "xml", "typescript", "tsx", "sql", "scss", "norg", "markdown", "lua" , "json", "jq", "javascript" , "jsdoc", "http", "html", "gitignore", "gitcommit", "git_rebase", "git_config", "css" },
-       sync_install = false,
-       auto_install = true,
-       highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-    }
-     end
-  }
+    "nvim-treesitter/nvim-treesitter",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+          "c",
+          "cpp",
+          "cmake",
+          "bash",
+          "c_sharp",
+          "rust",
+          "vim",
+          "yaml",
+          "org",
+          "xml",
+          "typescript",
+          "tsx",
+          "sql",
+          "scss",
+          "norg",
+          "markdown",
+          "lua",
+          "json",
+          "jq",
+          "javascript",
+          "jsdoc",
+          "http",
+          "html",
+          "gitignore",
+          "gitcommit",
+          "git_rebase",
+          "git_config",
+          "css",
+        },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+      }
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require "lint"
+
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        python = { "pylint" },
+      }
+
+      local lint_autogroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufwritePost", "InsertLeave" }, {
+        group = lint_autogroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>l", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local conform = require "conform"
+      conform.setup {
+        formatters_by_ft = {
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          javascriptreact = { "prettier" },
+          typescriptreact = { "prettier" },
+          css = { "prettier" },
+          html = { "prettier" },
+          json = { "prettier" },
+          yaml = { "prettier" },
+          markdown = { "prettier" },
+          graphql = { "prettier" },
+          lua = { "stylua" },
+          python = { "isort", "black" },
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        },
+      }
+
+      vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+        conform.format {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        }
+      end, { desc = "Format file or range (in visual mode)" })
+    end,
+  },
 }
 return plugins
