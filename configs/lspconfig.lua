1726702a194cb43abfc30e24444827ed8d75f6d7
Panos Sakkos <panos.sakkos@gmail.com>
2023-12-06 14:05:48 +0200

    add json schema of panther schema tests

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -82,6 +82,7 @@ lspconfig.yamlls.setup {
     yaml = {
       schemas = {
         ["https://panther-community-us-east-1.s3.amazonaws.com/latest/logschema/schema.json"] = "internal/log_analysis/yamlschemas/schemas/**",
+        ["./internal/log_analysis/log_processor/logtypes/logtesting/schema-tests.json"] = "internal/log_analysis/yamlschemas/schemas/**/*_tests.yml",
         ["https://raw.githubusercontent.com/panther-labs/panther_analysis_tool/main/panther_analysis_tool/detection_schemas/analysis_config_schema.json"] = {
           "rules/**",
           "policies/**",
