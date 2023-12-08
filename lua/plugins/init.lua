606ae455801a2636241f0aa457d0c24a908bff56
Sidhanth Rathod <siduck@tutanota.com>
2023-12-08 20:38:46 +0530

    performance: use uv.cwd() instead of fn.expand
    benchmarked luv's cwd and it seems to be 20x faster than the expand function

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -96,7 +96,7 @@ local default_plugins = {
       vim.api.nvim_create_autocmd({ "BufRead" }, {
         group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
         callback = function()
-          vim.fn.jobstart({"git", "-C", vim.fn.expand "%:p:h", "rev-parse"},
+          vim.fn.jobstart({"git", "-C", vim.loop.cwd(), "rev-parse"},
             {
               on_exit = function(_, return_code)
                 if return_code == 0 then
