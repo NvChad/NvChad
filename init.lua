6c0b78bbba8ed3a75c2a6c8f218755ed41d0d5f2
Panos Sakkos <panos.sakkos@gmail.com>
2023-12-08 16:00:36 +0200

    change color column to 100

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ local opt = vim.opt
 
 opt.backup = false
 opt.swapfile = false
-opt.colorcolumn = "80"
+opt.colorcolumn = "100"
 opt.relativenumber = true
 opt.list = true
 opt.listchars = "tab:➝ ,lead:·,space:·,trail:·,nbsp:+,eol:¬"
