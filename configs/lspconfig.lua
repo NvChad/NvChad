7801add8293b31fafe1d770d066604a796244589
Panos Sakkos <panos.sakkos@gmail.com>
2023-12-13 11:57:45 +0200

    updated Schema JSON schema path

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -81,7 +81,7 @@ lspconfig.yamlls.setup {
   settings = {
     yaml = {
       schemas = {
-        ["https://panther-community-us-east-1.s3.amazonaws.com/latest/logschema/schema.json"] = "internal/log_analysis/yamlschemas/schemas/**",
+        ["https://panther-community-us-east-1.s3.amazonaws.com/latest/logschema/schema.json"] = "internal/log_analysis/yamlschemas/schemas/**/*.yml",
         ["https://panther-community-us-east-1.s3.amazonaws.com/latest/logschema/schema-tests.json"] = "internal/log_analysis/yamlschemas/schemas/**/*_tests.yml",
         ["https://raw.githubusercontent.com/panther-labs/panther_analysis_tool/main/panther_analysis_tool/detection_schemas/analysis_config_schema.json"] = {
           "rules/**",
