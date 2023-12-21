1a69462f5581809bac39540f0bf4884d81070602
Panos Sakkos <panos.sakkos@gmail.com>
2023-12-21 16:17:58 +0200

    disable markdown line length in tables and code blocks

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -1,35 +1,40 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local opts = {
-  sources = {
-    null_ls.builtins.formatting.beautysh,
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.clang_format,
-    null_ls.builtins.formatting.gofmt,
-    null_ls.builtins.formatting.goimports,
-    null_ls.builtins.diagnostics.markdownlint,
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.rubyfmt,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.terraform_fmt,
-    null_ls.builtins.formatting.yamlfmt,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method "textDocument/formatting" then
-      vim.api.nvim_clear_autocmds {
-        group = augroup,
-        buffer = bufnr,
-      }
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format { bufnr = bufnr }
-        end,
-      })
-    end
-  end,
+	sources = {
+		null_ls.builtins.diagnostics.markdownlint.with({
+			extra_args = {
+				"--config",
+				vim.fn.expand("~/.config/nvim/configs/markdownlint.yaml"),
+			},
+		}),
+		null_ls.builtins.formatting.beautysh,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.clang_format,
+		null_ls.builtins.formatting.gofmt,
+		null_ls.builtins.formatting.goimports,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.rubyfmt,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.terraform_fmt,
+		null_ls.builtins.formatting.yamlfmt,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({
+				group = augroup,
+				buffer = bufnr,
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 }
 
 return opts
