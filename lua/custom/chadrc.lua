3831b2b9a56b8582b5868aa6010489945fc1c601
Panos Sakkos <panos.sakkos@gmail.com>
2023-12-22 11:16:52 +0200

    format lua code

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -2,14 +2,14 @@
 local M = {}
 
 M.ui = {
-	theme = "everforest_light",
-	statusline = {
-		theme = "default",
-		separator_style = "block",
-	},
+  theme = "everforest_light",
+  statusline = {
+    theme = "default",
+    separator_style = "block",
+  },
 }
 M.plugins = "custom.plugins"
-M.mappings = require("custom.mappings")
+M.mappings = require "custom.mappings"
 
 vim.g.toggle_theme_icon = ""
 
