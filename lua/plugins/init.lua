3f9b6727bf851db05022cca645584862ca05f676
Bruno Krugel <brunokrugel@gmail.com>
2023-12-23 19:11:56 -0300

    remove fzf native

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -234,7 +234,7 @@ local default_plugins = {
 
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
     cmd = "Telescope",
     init = function()
       require("core.utils").load_mappings "telescope"
