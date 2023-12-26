8a2f827ff5c484a53aac24b96f4b7a25a4ab850e
Panos Sakkos <panos.sakkos@gmail.com>
2023-12-26 02:20:48 +0200

    remove unused variable

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -2,7 +2,6 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local util = require "lspconfig/util"
 
 lspconfig.bashls.setup {
   on_attach = on_attach,
