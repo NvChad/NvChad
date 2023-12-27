e770ad5d1ca855143c7b7d7c375820a60e5a753c
siduck <siduck@tutanota.com>
2023-12-27 19:49:31 +0530

    check for empty/nil mason ensure_installed table #2580

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -133,7 +133,9 @@ local default_plugins = {
 
       -- custom nvchad cmd to install all mason binaries listed
       vim.api.nvim_create_user_command("MasonInstallAll", function()
-        vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+        if opts.ensure_installed and #opts.ensure_installed > 0 then
+          vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+        end
       end, {})
 
       vim.g.mason_binaries_list = opts.ensure_installed
