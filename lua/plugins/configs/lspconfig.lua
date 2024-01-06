c80f3f0501800d02b0085ecc1f79bfc64327d01e
siduck <siduck@tutanota.com>
2024-01-06 21:00:58 +0530

    Avoid disabling lsp formatting capabilities
    this was used so we could use null-ls as default formatter but as its dead i have replaced it with conform in the example_config. check https://github.com/NvChad/example_config/commit/a321ba986da839dc82eec52d072f1c16737d63b8

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -7,9 +7,6 @@ local utils = require "core.utils"
 -- export on_attach & capabilities for custom lspconfigs
 
 M.on_attach = function(client, bufnr)
-  client.server_capabilities.documentFormattingProvider = false
-  client.server_capabilities.documentRangeFormattingProvider = false
-
   utils.load_mappings("lspconfig", { buffer = bufnr })
 
   if client.server_capabilities.signatureHelpProvider then
