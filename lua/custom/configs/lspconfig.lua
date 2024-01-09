e63b5d920484d50b98f6fec86a3712f92c524412
Loc Le Phat <lpl212757@gmail.com>
2024-01-10 04:10:43 +0700

    MOD - Add sonarlint config

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -54,4 +54,23 @@ lspconfig.clangd.setup {
     "--offset-encoding=utf-16",
   },
 }
+
+require("sonarlint").setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  server = {
+    cmd = {
+      "C:\\Users\\Admin\\AppData\\Local\\nvim-data\\mason\\packages\\sonarlint-language-server\\sonarlint-language-server.cmd",
+      "-stdio",
+      "-analyzers",
+       "C:\\Users\\Admin\\AppData\\Local\\nvim-data\\mason\\share\\sonarlint-analyzers\\sonarpython.jar",
+       "C:\\Users\\Admin\\AppData\\Local\\nvim-data\\mason\\share\\sonarlint-analyzers\\sonarlintomnisharp.jar"
+    },
+
+  },
+  filetypes = {
+    'python',
+    'cs'
+  },
+}
 -- End user config
