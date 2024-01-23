f414efa79eb0db2cb821f1c5d194b8c03df254ab
Loc Le Phat <lpl212757@gmail.com>
2024-01-24 02:11:17 +0700

    MOD - Add noice extension for commandline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,6 +9,46 @@ local default_plugins = {
   --   "mfussenegger/nvim-jdtls",
   --   event = { "LspAttach" },
   -- },
+  --
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+      },
+    config = function()
+    require("noice").setup({
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    })
+    end,
+  },
+  {
+    "stevearc/dressing.nvim",
+    lazy = false,
+    config = function()
+      require("dressing").setup {}
+    end,
+  },
   {
     "nvim-pack/nvim-spectre",
     event = { "BufEnter" },
