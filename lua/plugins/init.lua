18c75eb2db18bd2a0d4132fc3b54efebd7528193
Le Phat Loc <34123125+lpl212757@users.noreply.github.com>
2024-01-24 10:58:56 +0700

    MOD - Remove noice and back to use NVChad/base46

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,38 +11,6 @@ local default_plugins = {
   -- },
   --
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-      },
-    config = function()
-    require("noice").setup({
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-      },
-    })
-    end,
-  },
-  {
     "stevearc/dressing.nvim",
     lazy = false,
     config = function()
@@ -75,6 +43,11 @@ local default_plugins = {
       }
     end,
   },
+  {
+    "tpope/vim-surround",
+    event = "BufEnter",
+  },
+  { "neo4j-contrib/cypher-vim-syntax", event = "BufEnter" },
   {
     "echasnovski/mini.nvim",
     event = "BufEnter",
@@ -124,7 +97,7 @@ local default_plugins = {
   "nvim-lua/plenary.nvim",
 
   {
-    "lpl212757/base46",
+    "NvChad/base46",
     branch = "v2.0",
     build = function()
       require("base46").load_all_highlights()
