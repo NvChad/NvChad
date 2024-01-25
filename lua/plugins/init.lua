7062b498f88cc2687c3c80fb9dc60e4509576c72
Le Phat Loc <34123125+lpl212757@users.noreply.github.com>
2024-01-25 09:47:49 +0700

    MOD - Remove incorrect config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -78,14 +78,6 @@ local default_plugins = {
     event = "BufEnter",
     config = function()
       require("mini.cursorword").setup {}
-    end,
-    config = function(_, opts)
-      require("colorizer").setup(opts)
-
-      -- execute colorizer as soon as possible
-      vim.defer_fn(function()
-        require("colorizer").attach_to_buffer(0)
-      end, 0)
     end
   },
   {
