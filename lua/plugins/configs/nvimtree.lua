200f4435629b32db05d1f447c6788cb42117d7ea
Le Phat Loc <34123125+lpl212757@users.noreply.github.com>
2024-01-25 09:51:41 +0700

    MOD - Flexible panel size of nvimtree

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -14,7 +14,7 @@ local options = {
     update_root = false,
   },
   view = {
-    adaptive_size = false,
+    adaptive_size = true,
     side = "left",
     width = 30,
     preserve_window_proportions = true,
