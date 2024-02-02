59082a8f28c8f33ee3cca5ac6932c15b5a3c53e8
siduck <siduck@tutanota.com>
2024-02-02 09:11:04 +0530

    add float opts for term feature

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -77,6 +77,14 @@ M.ui = {
 
   term = {
     sizes = { sp = 0.3, vsp = 0.2 },
+    float = {
+      relative = "editor",
+      row = 0.3,
+      col = 0.25,
+      width = 0.5,
+      height = 0.4,
+      border = "single",
+    },
     behavior = {
       auto_insert = true,
     },
