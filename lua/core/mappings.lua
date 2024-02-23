c32ccaf7ffa9b95c3b72b3fc49b4b1a267d89510
Sidhanth Rathod <siduck@tutanota.com>
2024-02-23 07:57:57 +0530

    Update mappings.lua

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -391,25 +391,18 @@ M.blankline = {
   n = {
     ["<leader>cc"] = {
       function()
-        local config = {
-          scope = {
-            exclude = {
-              language = {},
-              node_type = {},
-            },
-            include = {
-              node_type = {},
-            },
-            injected_languages = true,
-          },
-        }
+        local config = { scope = {} }
+        config.scope.exclude = { language = {}, node_type = {} }
+        config.scope.include = { node_type = {} }
+
         local node = require("ibl.scope").get(vim.api.nvim_get_current_buf(), config)
 
         if node then
           local start_row, _, end_row, _ = node:range()
+
           if start_row ~= end_row then
             vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { start_row + 1, 0 })
-            vim.cmd [[normal! _]]
+            vim.api.nvim_feedkeys("_", "n", true)
           end
         end
       end,
