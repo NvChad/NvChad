0dcd8a91b69f1fc0fe7064bd404b5390c1c164c5
Eduardo Bray <ebray187@gmail.com>
2024-02-26 04:56:38 -0300

    Remove Telescope unsupported layout_config key `results_width` (#2694)

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -21,7 +21,6 @@ local options = {
       horizontal = {
         prompt_position = "top",
         preview_width = 0.55,
-        results_width = 0.8,
       },
       vertical = {
         mirror = false,
