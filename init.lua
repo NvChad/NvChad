5d69a0d0f9f9e0b811a734d0adf1c751d3f2ce00
siduck <siduck@tutanota.com>
2024-02-29 21:50:47 +0530

    rm un-needed dofile call

--- init.lua
+++ init.lua
@@ -1,10 +1,6 @@
 require "core"
 
-local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
-
-if custom_init_path then
-  dofile(custom_init_path)
-end
+pcall(require, 'custom')
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
