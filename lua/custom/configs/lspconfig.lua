8c87227389a4e180a33cbbacb2097224c9eac21e
kakashi <trishiragolder69@gmail.com>
2024-03-06 05:36:42 +0530

    added deno lsp

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,12 +4,19 @@ local lspconfig=require("lspconfig");
 local configs=require("plugins.configs.lspconfig");
 
 
-
 lspconfig.clangd.setup({
   on_attach=on_attach,
   capabilities=capabilities,
   filetypes={"c","cpp"},
   cmd={"clangd"}
+});
+
+lspconfig.denols.setup({
+  on_attach=on_attach,
+  capabilities=capabilities,
+  root_dir=lspconfig.util.root_pattern("deno.json","deno.jsonc"),
+  filetypes={"javascript","typescript","javascriptreact","typescriptreact","javascript.jsx","typescript.tsx"},
+  cmd={"deno","lsp"}
 });
 
 
