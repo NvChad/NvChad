6b40fb590f9750e209fb8fb78d59d2e89d0748a6
siduck <siduck@tutanota.com>
2024-03-09 18:13:19 +0530

    use vim.o instead of vim.opt for non tb opts

--- lua/nvchad/options.lua
+++ lua/nvchad/options.lua
@@ -1,46 +1,46 @@
 local opt = vim.opt
+local o = vim.o
 local g = vim.g
 
 -------------------------------------- globals -----------------------------------------
--- g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 g.toggle_theme_icon = "   "
 
 -------------------------------------- options ------------------------------------------
-opt.laststatus = 3 -- global statusline
-opt.showmode = false
+o.laststatus = 3
+o.showmode = false
 
-opt.clipboard = "unnamedplus"
-opt.cursorline = true
-opt.cursorlineopt = "number"
+o.clipboard = "unnamedplus"
+o.cursorline = true
+o.cursorlineopt = "number"
 
 -- Indenting
-opt.expandtab = true
-opt.shiftwidth = 2
-opt.smartindent = true
-opt.tabstop = 2
-opt.softtabstop = 2
+o.expandtab = true
+o.shiftwidth = 2
+o.smartindent = true
+o.tabstop = 2
+o.softtabstop = 2
 
 opt.fillchars = { eob = " " }
-opt.ignorecase = true
-opt.smartcase = true
-opt.mouse = "a"
+o.ignorecase = true
+o.smartcase = true
+o.mouse = "a"
 
 -- Numbers
-opt.number = true
-opt.numberwidth = 2
-opt.ruler = false
+o.number = true
+o.numberwidth = 2
+o.ruler = false
 
 -- disable nvim intro
 opt.shortmess:append "sI"
 
-opt.signcolumn = "yes"
-opt.splitbelow = true
-opt.splitright = true
-opt.timeoutlen = 400
-opt.undofile = true
+o.signcolumn = "yes"
+o.splitbelow = true
+o.splitright = true
+o.timeoutlen = 400
+o.undofile = true
 
 -- interval for writing swap file to disk, also used by gitsigns
-opt.updatetime = 250
+o.updatetime = 250
 
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
