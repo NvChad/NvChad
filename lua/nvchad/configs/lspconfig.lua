bbc2cbaae65881f6d505fd8a6ef0e2453f7a3f73
siduck <siduck@tutanota.com>
2024-03-09 15:41:16 +0530

    rm including types in default lspconfig lua_lsp setup

--- lua/nvchad/configs/lspconfig.lua
+++ lua/nvchad/configs/lspconfig.lua
@@ -80,8 +80,6 @@ M.defaults = function()
           library = {
             [vim.fn.expand "$VIMRUNTIME/lua"] = true,
             [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-            [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
-            [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
           },
           maxPreload = 100000,
           preloadFileSize = 10000,
