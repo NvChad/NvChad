ccf6bc397f1eede517e8743859d8dfb900da9b2c
siduck <siduck@tutanota.com>
2024-03-10 08:55:15 +0530

    rm reference for example_config in bootstrap

--- lua/core/bootstrap.lua
+++ lua/core/bootstrap.lua
@@ -40,21 +40,13 @@ M.gen_chadrc_template = function()
   local path = fn.stdpath "config" .. "/lua/custom"
 
   if fn.isdirectory(path) ~= 1 then
-    local input = vim.env.NVCHAD_EXAMPLE_CONFIG or fn.input "Do you want to install example custom config? (y/N): "
+    -- use very minimal chadrc
+    fn.mkdir(path, "p")
 
-    if input:lower() == "y" then
-      M.echo "Cloning example custom config repo..."
-      shell_call { "git", "clone", "--depth", "1", "https://github.com/NvChad/example_config", path }
-      fn.delete(path .. "/.git", "rf")
-    else
-      -- use very minimal chadrc
-      fn.mkdir(path, "p")
-
-      local file = io.open(path .. "/chadrc.lua", "w")
-      if file then
-        file:write "---@type ChadrcConfig\nlocal M = {}\n\nM.ui = { theme = 'onedark' }\n\nreturn M"
-        file:close()
-      end
+    local file = io.open(path .. "/chadrc.lua", "w")
+    if file then
+      file:write "---@type ChadrcConfig\nlocal M = {}\n\nM.ui = { theme = 'onedark' }\n\nreturn M"
+      file:close()
     end
   end
 end
