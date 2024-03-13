f4e642fa3694ccb7f8f705b072ab33e1f6b01d9c
KorigamiK <72932688+KorigamiK@users.noreply.github.com>
2024-03-13 20:50:38 +0000

    add more gitsigns mappings

--- lua/nvchad/configs/gitsigns.lua
+++ lua/nvchad/configs/gitsigns.lua
@@ -20,6 +20,28 @@ local options = {
     map("n", "<leader>rh", gs.reset_hunk, opts "Reset Hunk")
     map("n", "<leader>ph", gs.preview_hunk, opts "Preview Hunk")
     map("n", "<leader>gb", gs.blame_line, opts "Blame Line")
+    map("n", "<leader>td", gs.toggle_deleted, opts "Toggle deleted")
+    
+    map("n", "]c", function()
+      if vim.wo.diff then
+        return "]c"
+      end
+      vim.schedule(function()
+        require("gitsigns").next_hunk()
+      end)
+      return "<Ignore>"
+    end, { desc = "Jump to next hunk", expr = true })
+    
+    map("n", "[c", function()
+      if vim.wo.diff then
+        return "[c"
+      end
+      vim.schedule(function()
+        require("gitsigns").prev_hunk()
+      end)
+      return "<Ignore>"
+    end, { desc = "Jump to prev hunk", expr = true })
+    
   end,
 }
 
