41adc6ce763d7f195393d89720b49ce976cfee6e
groveer <groveer@qq.com>
2024-03-18 11:47:11 +0800

    feat: restore cursor position from last time

--- lua/nvchad/autocmds.lua
+++ lua/nvchad/autocmds.lua
@@ -67,3 +67,19 @@ vim.api.nvim_create_autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
     end
   end,
 })
+
+-- restore cursor position
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
