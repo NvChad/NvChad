82413d957e19c9f6ec1dd4f9a553795342829784
siduck <siduck@tutanota.com>
2024-03-24 06:07:53 +0530

    disable colornames in nvim-colorizer

--- lua/nvchad/plugins/ui.lua
+++ lua/nvchad/plugins/ui.lua
@@ -20,6 +20,7 @@ return {
   {
     "NvChad/nvim-colorizer.lua",
     event = "User FilePost",
+    opts = { user_default_options = { names = false } },
     config = function(_, opts)
       require("colorizer").setup(opts)
 
