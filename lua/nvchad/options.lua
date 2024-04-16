1295e9eafa3a35ed48afd95b2fdc4ce8ce14f8de
Basil Feitknecht <114588139+bs10x@users.noreply.github.com>
2024-04-16 02:06:54 +0200

    update options.lua (#2790)
    remove unneccessary ```vim.*```

--- lua/nvchad/options.lua
+++ lua/nvchad/options.lua
@@ -49,10 +49,10 @@ opt.whichwrap:append "<>[]hl"
 -- g.mapleader = " "
 
 -- disable some default providers
-vim.g["loaded_node_provider"] = 0
-vim.g["loaded_python3_provider"] = 0
-vim.g["loaded_perl_provider"] = 0
-vim.g["loaded_ruby_provider"] = 0
+g["loaded_node_provider"] = 0
+g["loaded_python3_provider"] = 0
+g["loaded_perl_provider"] = 0
+g["loaded_ruby_provider"] = 0
 
 -- add binaries installed by mason.nvim to path
 local is_windows = vim.fn.has("win32") ~= 0
