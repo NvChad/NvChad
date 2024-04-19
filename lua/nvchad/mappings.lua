f8e6c59985f1d5f820f051395e88064a8d16ef2a
siduck <siduck@tutanota.com>
2024-04-19 06:57:32 +0530

    rm size prop from term toggle funcs
    Doing so will make the term module take the size value from the chadrc config | check  https://github.com/NvChad/ui/issues/267

--- lua/nvchad/mappings.lua
+++ lua/nvchad/mappings.lua
@@ -86,20 +86,20 @@ map("t", "<C-x>", "<C-\\><C-N>", { desc = "Terminal Escape terminal mode" })
 
 -- new terminals
 map("n", "<leader>h", function()
-  require("nvchad.term").new { pos = "sp", size = 0.3 }
+  require("nvchad.term").new { pos = "sp" }
 end, { desc = "Terminal New horizontal term" })
 
 map("n", "<leader>v", function()
-  require("nvchad.term").new { pos = "vsp", size = 0.3 }
+  require("nvchad.term").new { pos = "vsp" }
 end, { desc = "Terminal New vertical window" })
 
 -- toggleable
 map({ "n", "t" }, "<A-v>", function()
-  require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm", size = 0.3 }
+  require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
 end, { desc = "Terminal Toggleable vertical term" })
 
 map({ "n", "t" }, "<A-h>", function()
-  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm", size = 0.3 }
+  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
 end, { desc = "Terminal New horizontal term" })
 
 map({ "n", "t" }, "<A-i>", function()
