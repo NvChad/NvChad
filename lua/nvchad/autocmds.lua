53df5e175aede4259e7b3dfb0e67936c64dafee7
Hassan Ait Nacer <108420918+hassanaitnacer@users.noreply.github.com>
2024-05-02 00:46:58 +0100

    use pre-defined local variable autocmd (#2816)

--- lua/nvchad/autocmds.lua
+++ lua/nvchad/autocmds.lua
@@ -44,7 +44,7 @@ autocmd("BufWritePost", {
 })
 
 -- user event that loads after UIEnter + only if file buf is there
-vim.api.nvim_create_autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
+autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
   group = vim.api.nvim_create_augroup("NvFilePost", { clear = true }),
   callback = function(args)
     local file = vim.api.nvim_buf_get_name(args.buf)
