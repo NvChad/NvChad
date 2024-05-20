066b3f66c3852b9f1228c5a3699028887f39a08d
kochser <s225169@student.dtu.dk>
2024-05-20 21:30:12 +0200

    Made lsp config work for all windows pc

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -12,26 +12,28 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- local mason_path = vim.fn.stdpath('data') .. "/mason/"
--- use gsup("\\", "/") to substitute path
+-- Special setup for lsps
+
+local mason_path = os.getenv("LOCALAPPDATA") .. "\\nvim-data\\mason"
+local mason_path_unix = string.gsub(mason_path, "\\", "/")
+
+--
+-- powershell_es
+--
 
 lspconfig.powershell_es.setup {
-  -- bundle_path = "C:/Users/casper.koch/AppData/Local/nvim-data/mason/packages/powershell-editor-services",
-  bundle_path = "C:/Users/casper.koch/AppData/Local/nvim-data/mason/packages/powershell-editor-services",
+  bundle_path = mason_path_unix .. "/packages/powershell-editor-services" ,
   on_attach = on_attach,
   capabilities = capabilities,
 }
 
+--
+-- omnisharp 
+--
 
--- Special setup for omnisharp
 local pid = vim.fn.getpid()
 lspconfig.omnisharp.setup({
   on_attach = on_attach,
   capabilities = capabilities,
-  cmd = { "C:\\Users\\kochc\\AppData\\Local\\nvim-data\\mason\\bin\\omnisharp.cmd", "--languageserver" , "--hostPID", tostring(pid) },
+  cmd = { mason_path .. "\\bin\\omnisharp.cmd", "--languageserver" , "--hostPID", tostring(pid) },
 })
-
--- require('ionide').setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
--- }
