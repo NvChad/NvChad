4c062e8b65890b8a43a1613dfd7046d3a94861ce
Ender Fletcher <e2fletcher@gmail.com>
2024-06-02 14:36:10 -0400

    Change lsp default mapping

--- lua/nvchad/configs/lspconfig.lua
+++ lua/nvchad/configs/lspconfig.lua
@@ -11,21 +11,21 @@ M.on_attach = function(client, bufnr)
   map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
   map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
   map("n", "gi", vim.lsp.buf.implementation, opts "Go to implementation")
-  map("n", "<leader>sh", vim.lsp.buf.signature_help, opts "Show signature help")
-  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
-  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
+  map("n", "<leader>lh", vim.lsp.buf.signature_help, opts "Show signature help")
+  -- map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
+  -- map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
 
-  map("n", "<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts "List workspace folders")
+  -- map("n", "<leader>wl", function()
+  --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  -- end, opts "List workspace folders")
 
   map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
 
-  map("n", "<leader>ra", function()
-    require "nvchad.lsp.renamer"()
-  end, opts "NvRenamer")
+  map("n", "<leader>lr", function()
+    require "nvchad.lsp.renamer" ()
+  end, opts "Rename current symbol")
 
-  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Code action")
+  map({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts "LSP Code action")
   map("n", "gr", vim.lsp.buf.references, opts "Show references")
 
   -- setup signature popup
