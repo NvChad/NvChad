94fd6d791ffb340f007ee6b6c4223836b1d9f203
Ender Fletcher <e2fletcher@gmail.com>
2024-06-03 18:15:38 -0400

    perf(gitsigns): Add move hunk mapping

--- lua/nvchad/configs/gitsigns.lua
+++ lua/nvchad/configs/gitsigns.lua
@@ -16,16 +16,17 @@ local options = {
     end
 
     local map = vim.keymap.set
-
-    map({ "n" }, "<leader>gL", function()
+    map("n", "]g", gs.next_hunk, opts "Next hunk")
+    map("n", "[g", gs.prev_hunk, opts "Prev hunk")
+    map("n", "<leader>gL", function()
       gs.blame_line { full = true }
     end, opts "Blame full")
-    map({ "n" }, "<leader>gl", gs.blame_line, opts "Blame line")
-    map({ "n" }, "<leader>gh", gs.preview_hunk_inline, opts "Preview hunk")
-    map({ "n" }, "<leader>gd", gs.diffthis, opts "Buffer diff")
-    map({ "n" }, "<leader>gr", gs.reset_hunk, opts "Reset hunk")
-    map({ "n" }, "<leader>gs", gs.stage_hunk, opts "Stage hunk")
-    map({ "n" }, "<leader>gu", gs.undo_stage_hunk, opts "Unstage git hunk")
+    map("n", "<leader>gl", gs.blame_line, opts "Blame line")
+    map("n", "<leader>gh", gs.preview_hunk_inline, opts "Preview hunk")
+    map("n", "<leader>gd", gs.diffthis, opts "Buffer diff")
+    map("n", "<leader>gr", gs.reset_hunk, opts "Reset hunk")
+    map("n", "<leader>gs", gs.stage_hunk, opts "Stage hunk")
+    map("n", "<leader>gu", gs.undo_stage_hunk, opts "Unstage hunk")
   end,
 }
 
