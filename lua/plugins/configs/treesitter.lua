c70b000cdcebe788c1aa3a7fdd47ddffa1a1418a
kanashi <kanashi@me.com>
2024-06-10 23:10:14 +0200

    Autotag for HTML

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -7,6 +7,13 @@ local options = {
   },
 
   indent = { enable = true },
+  autotag = {
+    enable = true,
+    enable_ranme = true,
+    enable_close = true,
+    enable_close_on_slash = true,
+    filetyoes = {"html", "xml"},
+  },
 }
 
 return options
