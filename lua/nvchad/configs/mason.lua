bf477a6bbaa13348007482fff0416f3cb92ec55c
siduck <siduck@tutanota.com>
2024-07-14 08:18:18 +0530

    mason config: remove default options & add better icons

--- lua/nvchad/configs/mason.lua
+++ lua/nvchad/configs/mason.lua
@@ -5,20 +5,9 @@ local options = {
 
   ui = {
     icons = {
-      package_pending = " ",
-      package_installed = "󰄳 ",
-      package_uninstalled = " 󰚌",
-    },
-
-    keymaps = {
-      toggle_server_expand = "<CR>",
-      install_server = "i",
-      update_server = "u",
-      check_server_version = "c",
-      update_all_servers = "U",
-      check_outdated_servers = "C",
-      uninstall_server = "X",
-      cancel_installation = "<C-c>",
+      package_pending = " ",
+      package_installed = " ",
+      package_uninstalled = " ",
     },
   },
 
