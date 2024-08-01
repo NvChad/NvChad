c892cfedb247cf829720dda80d3b32d7535a96ac
siduck <siduck@tutanota.com>
2024-08-01 09:43:02 +0530

    rm some duplicate default opts of nvimtree config

--- lua/nvchad/configs/nvimtree.lua
+++ lua/nvchad/configs/nvimtree.lua
@@ -1,75 +1,33 @@
 dofile(vim.g.base46_cache .. "nvimtree")
 
 local options = {
-  filters = {
-    dotfiles = false,
-  },
+  filters = { dotfiles = true },
   disable_netrw = true,
-  hijack_netrw = true,
   hijack_cursor = true,
-  hijack_unnamed_buffer_when_opening = false,
   sync_root_with_cwd = true,
   update_focused_file = {
     enable = true,
     update_root = false,
   },
   view = {
-    adaptive_size = false,
-    side = "left",
     width = 30,
     preserve_window_proportions = true,
-  },
-  git = {
-    enable = true,
-    ignore = true,
-  },
-  filesystem_watchers = {
-    enable = true,
-  },
-  actions = {
-    open_file = {
-      resize_window = true,
-    },
   },
   renderer = {
     root_folder_label = false,
     highlight_git = true,
-    highlight_opened_files = "none",
-
-    indent_markers = {
-      enable = true,
-    },
-
+    indent_markers = { enable = true },
     icons = {
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = true,
-      },
-
       glyphs = {
         default = "󰈚",
-        symlink = "",
         folder = {
           default = "",
           empty = "",
           empty_open = "",
           open = "",
           symlink = "",
-          symlink_open = "",
-          arrow_open = "",
-          arrow_closed = "",
         },
-        git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
-        },
+        git = { unmerged = "" },
       },
     },
   },
