0e42ae705a57e5558344b6e37c9863fac954284b
siduck <siduck@tutanota.com>
2024-08-27 08:16:22 +0530

    replace: nvim-colorizer plugin with ui.colorify (BREAKING CHANGE)
    https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua#L106

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -162,26 +162,6 @@ return {
   },
 
   {
-    "NvChad/nvim-colorizer.lua",
-    event = "User FilePost",
-    opts = {
-      user_default_options = { names = false },
-      filetypes = {
-        "*",
-        "!lazy",
-      },
-    },
-    config = function(_, opts)
-      require("colorizer").setup(opts)
-
-      -- execute colorizer as soon as possible
-      vim.defer_fn(function()
-        require("colorizer").attach_to_buffer(0)
-      end, 0)
-    end,
-  },
-
-  {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPost", "BufNewFile" },
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
