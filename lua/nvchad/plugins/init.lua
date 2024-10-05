db9d45cc3da403ad6213e7fc439dc7edf62a945e
siduck <siduck@tutanota.com>
2024-10-05 18:43:25 +0530

    rm: un-needed mason.opts.ensure_installed warning

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -89,22 +89,6 @@ return {
     opts = function()
       return require "nvchad.configs.mason"
     end,
-    config = function(_, opts)
-      if opts.ensure_installed then
-        vim.api.nvim_echo({
-          { "\n   ensure_installed has been removed! use M.mason.pkgs table in your chadrc.\n", "WarningMsg" },
-          { "   https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua#L85 \n\n", "FloatBorder" },
-          {
-            "   MasonInstallAll will automatically install all mason packages of tools configured in your plugins. \n",
-            "healthSuccess",
-          },
-          { "   Currently supported plugins are : lspconfig, nvim-lint, conform. \n", "Added" },
-          { "   So dont add them in your chadrc as MasonInstallAll automatically installs them! \n", "Changed" },
-        }, false, {})
-      end
-
-      require("mason").setup(opts)
-    end,
   },
 
   {
