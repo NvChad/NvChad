94ddf896297c1baf91ff635f428a479de91dca00
kamack38 <64226248+kamack38@users.noreply.github.com>
2025-04-16 12:15:04 +0200

    feat: 🎸 Support range formatting

--- lua/nvchad/mappings.lua
+++ lua/nvchad/mappings.lua
@@ -21,7 +21,7 @@ map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
 map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
 map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "toggle nvcheatsheet" })
 
-map("n", "<leader>fm", function()
+map({ "n", "v" }, "<leader>fm", function()
   require("conform").format { lsp_fallback = true }
 end, { desc = "general format file" })
 
