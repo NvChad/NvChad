d62c9537b22ff134b2c9bfe5e60504c009fe75b9
Sidhanth Rathod <siduck@tutanota.com>
2025-07-12 20:16:07 +0530

    Update lspconfig.lua

--- lua/nvchad/configs/lspconfig.lua
+++ lua/nvchad/configs/lspconfig.lua
@@ -59,9 +59,7 @@ M.defaults = function()
 
   local lua_lsp_settings = {
     Lua = {
-      runtime = {
-        version = "LuaJIT",
-      },
+      runtime = { version = "LuaJIT" },
       workspace = {
         library = {
           vim.fn.expand "$VIMRUNTIME/lua",
