d38feb78081f85594c2b6c17154a42ed810121ff
dingbat-rascal <dingbat.rascal@gmail.com>
2025-09-06 03:29:10 -0700

    Update lspconfig.lua
    client.supports_method is deprecated. Feature will be removed in Nvim 0.13. Switched to client:supports_method instead.

--- lua/nvchad/configs/lspconfig.lua
+++ lua/nvchad/configs/lspconfig.lua
@@ -22,7 +22,7 @@ end
 
 -- disable semanticTokens
 M.on_init = function(client, _)
-  if client.supports_method "textDocument/semanticTokens" then
+  if client:supports_method "textDocument/semanticTokens" then
     client.server_capabilities.semanticTokensProvider = nil
   end
 end
