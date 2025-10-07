bc81aabf3a5a6b6cf2e0c2566fbf37c0dca7608f
leonardespi <leonardespicass@gmail.com>
2025-10-07 13:02:25 -0600

    fix: migrate lua_ls config

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -40,7 +40,7 @@ M.capabilities.textDocument.completion.completionItem = {
   },
 }
 
-require("lspconfig").lua_ls.setup {
+vim.lsp.config("lua_ls", {
   on_init = M.on_init,
   on_attach = M.on_attach,
   capabilities = M.capabilities,
@@ -62,6 +62,8 @@ require("lspconfig").lua_ls.setup {
       },
     },
   },
-}
+})
+
+vim.lsp.enable({ "lua_ls" })
 
 return M
