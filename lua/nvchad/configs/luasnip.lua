339d54487df949e418353cc8e37553b4de0616b1
ddarfantasy <75518399+ddarfantasy@users.noreply.github.com>
2025-10-25 12:52:50 -0500

    Revert: fix: remove un-needed luasnip autocmd NvChad/ui#482

--- lua/nvchad/configs/luasnip.lua
+++ lua/nvchad/configs/luasnip.lua
@@ -9,3 +9,15 @@ require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snip
 -- lua format
 require("luasnip.loaders.from_lua").load()
 require("luasnip.loaders.from_lua").lazy_load { paths = vim.g.lua_snippets_path or "" }
+
+-- fix luasnip #258
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function()
+    if
+      require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+      and not require("luasnip").session.jump_active
+    then
+      require("luasnip").unlink_current()
+    end
+  end,
+})
