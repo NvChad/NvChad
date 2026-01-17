cbf112a80ce7df7e34eeb6d7c02611927e530b11
siduck <siduck@tutanota.com>
2026-01-17 12:08:45 -0500

    Merge changes from upstream/dev

--- lua/nvchad/autocmds.lua
+++ lua/nvchad/autocmds.lua
@@ -26,11 +26,17 @@ autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
   end,
 })
 
-autocmd("FileType", {
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
   callback = function()
-    -- TODO: nvim 0.12 will make error = false the default so we can remove it
-    if vim.treesitter.get_parser(nil, nil, { error = false }) then
-      vim.treesitter.start()
-    end
+    pcall(vim.treesitter.start)
   end,
 })
+
+local create_cmd = vim.api.nvim_create_user_command
+
+create_cmd("TSInstallAll", function()
+  local spec = require("lazy.core.config").plugins["nvim-treesitter"]
+  local opts = type(spec.opts) == "table" and spec.opts or {}
+  require("nvim-treesitter").install(opts.ensure_installed)
+end, {})
--- lua/nvchad/options.lua
+++ lua/nvchad/options.lua
@@ -55,6 +55,3 @@ local is_windows = vim.fn.has "win32" ~= 0
 local sep = is_windows and "\\" or "/"
 local delim = is_windows and ";" or ":"
 vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
-
--- treesitter indentation
-vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
