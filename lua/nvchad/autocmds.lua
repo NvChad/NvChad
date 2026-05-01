4e57dd00d3627327d23d1135e122ee0526cbe9f7
korigamik <korigamik@gmail.com>
2026-05-01 22:40:52 +0530

    fix(treesitter): nvim-treesitter crashes

--- lua/nvchad/autocmds.lua
+++ lua/nvchad/autocmds.lua
@@ -38,5 +38,8 @@ local create_cmd = vim.api.nvim_create_user_command
 create_cmd("TSInstallAll", function()
   local spec = require("lazy.core.config").plugins["nvim-treesitter"]
   local opts = type(spec.opts) == "table" and spec.opts or {}
-  require("nvim-treesitter").install(opts.ensure_installed)
-end, {})
+
+  if opts.ensure_installed then
+    require("nvim-treesitter").install(opts.ensure_installed, { summary = true })
+  end
+end, { desc = "Install all configured treesitter parsers" })
--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -156,11 +156,27 @@ return {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    build = ":TSUpdate | TSInstallAll",
+    lazy = false,
+    build = function()
+      local ts = require "nvim-treesitter"
+      local spec = require("lazy.core.config").plugins["nvim-treesitter"]
+      local opts = type(spec.opts) == "table" and spec.opts or {}
+
+      if opts.install_dir then
+        ts.setup { install_dir = opts.install_dir }
+      end
+
+      if opts.ensure_installed then
+        ts.install(opts.ensure_installed, { summary = true }):wait(300000)
+      end
+
+      ts.update(nil, { summary = true }):wait(300000)
+    end,
     opts = function()
       return require "nvchad.configs.treesitter"
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter").setup { install_dir = opts.install_dir }
     end,
   },
 }
